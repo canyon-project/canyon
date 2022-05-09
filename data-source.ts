bbc61e08d3c7b7d581bbf6d57dcc5575e3cfcb2d
zhangtao25 <tzhangm@trip.com>
2022-05-09 23:31:07 +0800

    add profile

--- data-source.ts
+++ data-source.ts
@@ -1,7 +1,21 @@
 import "reflect-metadata"
 import { DataSource } from "typeorm"
 import { User } from "./model/User"
+import {prepareInit} from "./utils/prepareInit";
 
+prepareInit()
+
+console.log('biao2',global.conf)
+const { datasource } = global.conf
+const { mysql, mongodb } = datasource
+console.log(__dirname,'__dirname')
 export const AppDataSource = new DataSource({
+    type: mysql.type,
+    host: mysql.host,
+    port: mysql.port,
+    username: mysql.username,
+    password: mysql.password,
+    database: mysql.database,
+    entities: [__dirname + '/model/*{.ts,.js}'],
     }
 )
--- main.ts
+++ main.ts
@@ -1,10 +1,20 @@
 import express from "express";
-import {AppDataSource} from "./data-source";
+// import {AppDataSource} from "./data-source";
 import api from './handler/api/api'
+import { prepareInit } from './utils/prepareInit'
 
-AppDataSource.initialize().then(async (MyDataSource) => {
-    const app = express();
-    app.use(express.json());
-    app.use('/', api.handler());
-    app.listen(3001);
-});
+async function bootstrap() {
+    // import {AppDataSource} from "./data-source";
+    const {AppDataSource} = await import('./data-source')
+    AppDataSource.initialize().then(async (MyDataSource) => {
+        const app = express();
+        app.use(express.json());
+        app.use('/', api.handler());
+        app.listen(3001);
+    });
+}
+
+bootstrap()
+
+
+
