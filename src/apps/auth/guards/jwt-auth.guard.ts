06bb4c425fb8cfc78af127a4b3824cb983d037af
zhangtao25 <tzhangm@trip.com>
2022-05-17 21:13:48 +0800

    使用pnpm

--- src/apps/auth/guards/jwt-auth.guard.ts
+++ src/apps/auth/guards/jwt-auth.guard.ts
@@ -9,7 +9,7 @@ export class JwtAuthGuard extends AuthGuard('jwt') {
     super()
   }
 
-  canActivate(context: ExecutionContext) {
+  canActivate(context: ExecutionContext): any {
     const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
       context.getHandler(),
       context.getClass(),
