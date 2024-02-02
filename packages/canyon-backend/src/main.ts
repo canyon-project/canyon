895ea8309014d08455c657abccb5076c9017c8ec
Allen Zhang (张涛) <wr_zhang25@163.com>
2024-02-02 15:50:16 +0800

    feat: add error message

--- packages/canyon-backend/src/main.ts
+++ packages/canyon-backend/src/main.ts
@@ -2,9 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { json } from 'express';
 import { ValidationPipe } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe());
   app.use(
     json({
--- packages/canyon-platform/src/main.tsx
+++ packages/canyon-platform/src/main.tsx
@@ -3,14 +3,40 @@ import 'antd/dist/reset.css';
 import './index.css';
 
 import { ApolloClient, ApolloProvider, createHttpLink, InMemoryCache } from '@apollo/client';
+import { onError } from '@apollo/client/link/error';
+import { message } from 'antd';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './App.tsx';
 
+// 创建一个error link来处理错误
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors) {
+    graphQLErrors.forEach(({ message: msg, locations, path }) => {
+      console.error(`[GraphQL error]: msg: ${msg}, Location: ${locations}, Path: ${path}`);
+      message.error(`[GraphQL error]: msg: ${msg}, Path: ${path}`);
+      if (
+        msg === 'Unauthorized' &&
+        window.location.pathname !== '/welcome' &&
+        window.location.pathname !== '/login'
+      ) {
+        localStorage.clear();
+        window.location.href = '/welcome';
+      }
+      // 在这里你可以执行自定义的操作，比如显示错误提示
+    });
+  }
+  if (networkError) {
+    console.error(`[Network error]: ${networkError}`);
+    // 在这里你可以执行自定义的操作，比如显示网络错误提示
+  }
+});
+
 // 创建一个http link来发送GraphQL请求
 const httpLink = createHttpLink({
   uri: '/graphql', // 你的GraphQL API的URL
+
   headers: {
     Authorization: `Bearer ` + (localStorage.getItem('token') || ''),
   },
@@ -18,7 +44,7 @@ const httpLink = createHttpLink({
 
 // 创建Apollo Client实例
 const client = new ApolloClient({
-  link: httpLink, // 将error link和http link组合起来
+  link: errorLink.concat(httpLink), // 将error link和http link组合起来
   cache: new InMemoryCache(),
 });
 
