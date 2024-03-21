00b59f56c66f48f96579f8310350588db8a29b67
Allen Zhang (张涛) <wr_zhang25@163.com>
2024-03-21 20:59:29 +0800

    upd

--- packages/canyon-backend/src/main.ts
+++ packages/canyon-backend/src/main.ts
@@ -9,6 +9,7 @@ async function bootstrap() {
   // 获取启动变量，动态修改.env文件
   const CFG = process.env.CFG;
   if (CFG) {
+    console.log('CFG', CFG);
     fs.writeFileSync('./.env', CFG, 'utf8');
   }
   const { AppModule } = await import('./app.module');
