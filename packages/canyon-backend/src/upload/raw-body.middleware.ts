9b5155820ad78d9ea45503516f18df030ddf40e0
Allen Zhang <37892968+zhangtao25@users.noreply.github.com>
2024-06-06 00:01:51 +0800

    feat: update

--- packages/canyon-backend/src/upload/raw-body.middleware.ts
+++ packages/canyon-backend/src/upload/raw-body.middleware.ts
@@ -6,13 +6,15 @@ import * as bodyParser from 'body-parser';
 @Injectable()
 export class RawBodyMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
-    bodyParser.json()(req, res, (errJson) => {
+    // Set the limit to 100MB for JSON and raw text bodies
+    const limit = '100mb';
+    bodyParser.json({ limit })(req, res, (errJson) => {
       if (errJson) {
         // Handle JSON parsing error
         console.error('Error parsing JSON:', errJson);
       }
 
-      bodyParser.raw({ type: 'text/plain' })(req, res, (errRaw) => {
+      bodyParser.raw({ type: 'text/plain', limit })(req, res, (errRaw) => {
         if (errRaw) {
           // Handle raw text parsing error
           console.error('Error parsing raw text:', errRaw);
--- packages/canyon-cli/README.md
+++ packages/canyon-cli/README.md
@@ -1,6 +1,6 @@
 debug
 
-node ~/Desktop/github/canyon-project/canyon/packages/canyon-cli/dist/bin/canyon.js --url http://localhost:8080
+node ~/github.com/canyon-project/canyon/packages/canyon-cli/dist/bin/canyon.js --url http://localhost:8080
 
 git tag -a v0.0.5 -m "Version 0.0.5"
 
