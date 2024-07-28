d8c9535f440ba7855a5dcd21c0a1865654040d23
Allen Zhang (张涛) <wr_zhang25@163.com>
2024-07-28 23:06:41 +0800

    feat: lint

--- packages/canyon-platform/src/App.tsx
+++ packages/canyon-platform/src/App.tsx
@@ -1,9 +1,9 @@
+import enUS from "antd/es/locale/en_US";
+import jaJP from "antd/es/locale/ja_JP";
+import zhCN from "antd/es/locale/zh_CN";
 import { useRoutes } from "react-router-dom";
 
 import routes from "~react-pages";
-import zhCN from "antd/es/locale/zh_CN";
-import enUS from "antd/es/locale/en_US";
-import jaJP from "antd/es/locale/ja_JP";
 
 const languages = {
   cn: zhCN,
