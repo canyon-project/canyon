d3bbb22f43bd4f33549419b460a0b38913dc49a5
Allen Zhang (张涛) <wr_zhang25@163.com>
2024-07-30 13:45:17 +0800

    feat: update

--- packages/canyon-platform/src/i18n.ts
+++ packages/canyon-platform/src/i18n.ts
@@ -1,13 +1,113 @@
 import i18n from "i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
-
-// import languages from '../languages.json';
 import cn from "../locales/cn.json";
 import en from "../locales/en.json";
 import ja from "../locales/ja.json";
-// import ko from '../locales/ko.json';
-// import tw from '../locales/tw.json';
+import kor from "../locales/kor.json";
+import fra from "../locales/fra.json";
+import spa from "../locales/spa.json";
+import th from "../locales/th.json";
+import ara from "../locales/ara.json";
+import ru from "../locales/ru.json";
+import pt from "../locales/pt.json";
+import de from "../locales/de.json";
+import it from "../locales/it.json";
+import el from "../locales/el.json";
+import nl from "../locales/nl.json";
+import pl from "../locales/pl.json";
+import bul from "../locales/bul.json";
+import est from "../locales/est.json";
+import dan from "../locales/dan.json";
+import fin from "../locales/fin.json";
+import cs from "../locales/cs.json";
+import rom from "../locales/rom.json";
+import slo from "../locales/slo.json";
+import swe from "../locales/swe.json";
+import hu from "../locales/hu.json";
+import cht from "../locales/cht.json";
+import vie from "../locales/vie.json";
+
+const resources = {
+  cn: {
+    translation: cn,
+  },
+  en: {
+    translation: en,
+  },
+  ja: {
+    translation: ja,
+  },
+  kor: {
+    translation: kor,
+  },
+  fra: {
+    translation: fra,
+  },
+  spa: {
+    translation: spa,
+  },
+  th: {
+    translation: th,
+  },
+  ara: {
+    translation: ara,
+  },
+  ru: {
+    translation: ru,
+  },
+  pt: {
+    translation: pt,
+  },
+  de: {
+    translation: de,
+  },
+  it: {
+    translation: it,
+  },
+  el: {
+    translation: el,
+  },
+  nl: {
+    translation: nl,
+  },
+  pl: {
+    translation: pl,
+  },
+  bul: {
+    translation: bul,
+  },
+  est: {
+    translation: est,
+  },
+  dan: {
+    translation: dan,
+  },
+  fin: {
+    translation: fin,
+  },
+  cs: {
+    translation: cs,
+  },
+  rom: {
+    translation: rom,
+  },
+  slo: {
+    translation: slo,
+  },
+  swe: {
+    translation: swe,
+  },
+  hu: {
+    translation: hu,
+  },
+  cht: {
+    translation: cht,
+  },
+  vie: {
+    translation: vie,
+  },
+};
 // const getIos = (code: string, languages: { code: string; iso: string }[]) =>
 //   languages.find((item: { code: string }) => item.code === code)?.iso || 'en-US';
 i18n
@@ -24,20 +124,7 @@ i18n
     interpolation: {
       escapeValue: false,
     },
-    resources: {
-      en: {
-        translation: en,
-      },
-      cn: {
-        translation: cn,
-      },
-      ja: {
-        translation: ja,
-      },
-      // ko: {
-      //   translation: ko,
-      // },
-    },
+    resources: resources,
     lng: localStorage.getItem("language") || "cn",
   });
 
