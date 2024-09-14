3f87dd57bd200df7953ccaf5dc1300341f0d3628
zhangtao25 <wr_zhang25@163.com>
2024-09-14 20:27:06 +0800

    feat: add rm script

---
+++ scripts/rm.js
@@ -0,0 +1,21 @@
+import fs from 'fs';
+import path from 'path';
+
+function deleteNodeModules(dir) {
+  const entries = fs.readdirSync(dir, { withFileTypes: true });
+  for (const entry of entries) {
+    const curPath = path.join(dir, entry.name);
+    if (entry.isDirectory()) {
+      const time = new Date().getTime();
+      if (entry.name === 'node_modules') {
+        fs.rmSync(curPath, { recursive: true, force: true })
+        console.log(`正在删除: ${entry.name}，耗时:${new Date().getTime() - time}`);
+      } else {
+        deleteNodeModules(curPath);
+      }
+    }
+  }
+}
+
+const rootDirectory = process.cwd();
+deleteNodeModules(rootDirectory);
