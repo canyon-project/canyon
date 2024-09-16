31ee2f10fabd96476b204a723dc6c243ee114cd0
zhangtao25 <wr_zhang25@163.com>
2024-09-16 20:37:18 +0800

    feat: update

--- packages/canyon-backend/src/main.ts
+++ packages/canyon-backend/src/main.ts
@@ -1,16 +1,16 @@
 import { NestFactory } from "@nestjs/core";
 import { json } from "express";
 import { ValidationPipe } from "@nestjs/common";
-import * as cookieParser from "cookie-parser";
 import * as dotenv from "dotenv";
 import * as path from "node:path";
+
 dotenv.config({
   path: path.resolve(__dirname, "../../../.env"),
 });
+
 async function bootstrap() {
   const { AppModule } = await import("./app.module");
   const app = await NestFactory.create(AppModule);
-  app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe());
   app.use(
     json({
@@ -18,6 +18,6 @@ async function bootstrap() {
     }),
   );
   app.enableCors();
-  await app.listen(8080);
+  await app.listen(process.env["PORT"] || 8080);
 }
 bootstrap();
