d212d27333ecac1b9a692fdbffbeab7733649b0e
Allen Zhang (张涛) <wr_zhang25@163.com>
2024-10-18 20:04:10 +0800

    feat: update

--- plugins/swc-plugin-canyon/src/lib.rs
+++ plugins/swc-plugin-canyon/src/lib.rs
@@ -6,11 +6,6 @@ use swc_plugin_macro::plugin_transform;
 
 #[plugin_transform]
 fn plugin(program: Program, metadata: TransformPluginProgramMetadata) -> Program {
-    // 获取文件路径并打印
-    if let Some(file_name) = metadata.get_context().filename.as_deref() {
-        println!("当前文件的路径: {}", file_name);
-    } else {
-        println!("无法获取文件路径");
-    }
+    println!("打印");
     program
 }
