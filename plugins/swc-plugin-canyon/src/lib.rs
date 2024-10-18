e6c33eed905b7fc88b8a6c66a6db7580cb2f795b
Allen Zhang (张涛) <wr_zhang25@163.com>
2024-10-18 19:41:14 +0800

    feat: update README

--- plugins/swc-plugin-canyon/src/lib.rs
+++ plugins/swc-plugin-canyon/src/lib.rs
@@ -5,7 +5,7 @@ use swc_ecma_ast::Program;
 use swc_plugin_macro::plugin_transform;
 
 #[plugin_transform]
-fn plugin(program: Program, _: TransformPluginProgramMetadata) -> Program {
+fn plugin(program: Program, metadata: TransformPluginProgramMetadata) -> Program {
     // 获取文件路径并打印
     if let Some(file_name) = metadata.get_context().filename.as_deref() {
         println!("当前文件的路径: {}", file_name);
