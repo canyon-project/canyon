12c77aa42192511745ae0abe5b3914c77f836d72
Allen Zhang (张涛) <wr_zhang25@163.com>
2024-10-19 22:26:08 +0800

    feat(swc-plugin-canyon): update

--- plugins/swc-plugin-canyon/src/lib.rs
+++ plugins/swc-plugin-canyon/src/lib.rs
@@ -1,10 +1,9 @@
 #![allow(clippy::not_unsafe_ptr_arg_deref)]
 
 use swc_core::plugin::proxies::TransformPluginProgramMetadata;
-use swc_ecma_ast::{Program, Module, ModuleItem, Script};
+use swc_ecma_ast::{Program, Module, ModuleItem, Script, Expr, ExprStmt, Stmt, Lit, Str};
 use swc_ecma_visit::{Fold, FoldWith};
 use swc_plugin_macro::plugin_transform;
-use swc_core::ecma::utils::quote_str;
 
 #[plugin_transform]
 fn plugin(program: Program, metadata: TransformPluginProgramMetadata) -> Program {
@@ -23,21 +22,34 @@ struct AddSimpleCode;
 
 impl Fold for AddSimpleCode {
     fn fold_module(&mut self, mut module: Module) -> Module {
-        let new_code = "(new Function('this')).__canyon__={tizhong:\"123\"};";
-        let new_item = ModuleItem::Stmt(swc_ecma_ast::Stmt::Expr(swc_ecma_ast::ExprStmt {
-            expr: Box::new(swc_ecma_ast::Expr::Lit(swc_ecma_ast::Lit::Str(quote_str(new_code)))),
+        // 构造 `(new Function('this')).__canyon__={tizhong:"123"};` 语句
+        let new_code = Expr::Lit(Lit::Str(Str {
+            value: "(new Function('this')).__canyon__={tizhong:\"123\"};".into(),
+            span: Default::default(),
+            raw: None,
+        }));
+
+        let new_item = ModuleItem::Stmt(Stmt::Expr(ExprStmt {
+            expr: Box::new(new_code),
             span: Default::default(),
         }));
+
         module.body.push(new_item);
         module
     }
 
     fn fold_script(&mut self, mut script: Script) -> Script {
-        let new_code = "(new Function('this')).__canyon__={tizhong:\"123\"};";
-        let new_item = swc_ecma_ast::Stmt::Expr(swc_ecma_ast::ExprStmt {
-            expr: Box::new(swc_ecma_ast::Expr::Lit(swc_ecma_ast::Lit::Str(quote_str(new_code)))),
+        let new_code = Expr::Lit(Lit::Str(Str {
+            value: "(new Function('this')).__canyon__={tizhong:\"123\"};".into(),
+            span: Default::default(),
+            raw: None,
+        }));
+
+        let new_item = Stmt::Expr(ExprStmt {
+            expr: Box::new(new_code),
             span: Default::default(),
         });
+
         script.body.push(new_item);
         script
     }
