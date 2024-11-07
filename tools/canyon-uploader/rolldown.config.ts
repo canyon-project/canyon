b7d22e398222dececb5870e2f3665ae986d17ff5
Allen Zhang <37892968+zhangtao25@users.noreply.github.com>
2024-11-07 23:52:09 +0800

    feat(canyon-uploader): update

--- tools/canyon-uploader/rolldown.config.ts
+++ tools/canyon-uploader/rolldown.config.ts
@@ -1,6 +1,12 @@
 import { defineConfig } from 'rolldown'
-
+const obj:{name:number} = {
+  name:1
+}
+console.log(obj.name)
 export default defineConfig({
   input: 'src/main.ts',
   platform: 'node',
+  output:{
+    format:'commonjs'
+  }
 })
--- tools/canyon-uploader/src/main.ts
+++ tools/canyon-uploader/src/main.ts
@@ -1,83 +1,84 @@
 import chalk from "chalk";
-// import { program } from "commander";
-// import { version } from "../package.json";
-//
-//
-// const accent = chalk.greenBright;
-//
-// async function crnAction(params: any, options: any): Promise<any>{
-//   console.log('????')
-// }
-//
-// /**
-//  * * Program Default Configuration
-//  */
-// const CLI_BEFORE_ALL_TXT = `canyon: The ${accent(
-//   "Canyon"
-// )} CLI - Version ${version} (${accent(
-//   "https://github.com/canyon-project/canyon"
-// )}) ${chalk.black.bold.bgYellowBright(" ALPHA ")} \n`;
-//
-// const CLI_AFTER_ALL_TXT = `\nFor more help, head on to ${accent(
-//   "https://github.com/canyon-project/canyon"
-// )}`;
-//
-// program
-//   .name("canyon")
-//   .version(version, "-v, --version", "see the current version of canyon-cli")
-//   .usage("[options or commands] arguments")
-//   .addHelpText("beforeAll", CLI_BEFORE_ALL_TXT)
-//   .addHelpText("after", CLI_AFTER_ALL_TXT)
-//   .configureHelp({
-//     optionTerm: (option) => accent(option.flags),
-//     subcommandTerm: (cmd) => accent(cmd.name(), cmd.usage()),
-//     argumentTerm: (arg) => accent(arg.name()),
-//   })
-//   .addHelpCommand(false)
-//   .showHelpAfterError(true);
-//
-//
-// /**
-//  * * CLI Commands
-//  */
-//
-// program
-//   .command("crn")
-//   .option(
-//     "--dsn <dsn>",
-//     "dsn of the canyon server"
-//   )
-//   .option(
-//     "--reporter <reporter>",
-//     "reporter auth token"
-//   )
-//   .option(
-//     "--project_id <project_id>",
-//     "id of the project"
-//   )
-//   .option("--commit_sha <commit_sha>", "commit sha of the project")
-//   .option("--branch <branch>", "branch of the project")
-//   .option("--buildId <buildId>", "buildId of the project")
-//   .option("--report_id <report_id>", "report id of the case")
-//   .option("--gitlab_url <gitlab_url>", "gitlab url of the project")
-//   .option("--payload_path <payload_path>", "payload_path")
-//   .option("--appid <appid>", "appid")
-//   .option("--module <module>", "module")
-//   .allowExcessArguments(false)
-//   .allowUnknownOption(false)
-//   .description("modify react native project code to adapt to canyon")
-//   .addHelpText(
-//     "after",
-//     `\nFor help, head on to ${accent(
-//       "https://github.com/canyon-project/canyon"
-//     )}`
-//   )
-//   .action(async (params, options) => await crnAction(params, options));
-//
+import { program } from "commander";
+import { version } from "../package.json";
+
+
+const accent = chalk.greenBright;
+
+async function crnAction(params: any, options: any): Promise<any>{
+  console.log('????')
+  return ['123']
+}
+
+/**
+ * * Program Default Configuration
+ */
+const CLI_BEFORE_ALL_TXT = `canyon: The ${accent(
+  "Canyon"
+)} CLI - Version ${version} (${accent(
+  "https://github.com/canyon-project/canyon"
+)}) ${chalk.black.bold.bgYellowBright(" ALPHA ")} \n`;
+
+const CLI_AFTER_ALL_TXT = `\nFor more help, head on to ${accent(
+  "https://github.com/canyon-project/canyon"
+)}`;
+
+program
+  .name("canyon")
+  .version(version, "-v, --version", "see the current version of canyon-cli")
+  .usage("[options or commands] arguments")
+  .addHelpText("beforeAll", CLI_BEFORE_ALL_TXT)
+  .addHelpText("after", CLI_AFTER_ALL_TXT)
+  .configureHelp({
+    optionTerm: (option) => accent(option.flags),
+    subcommandTerm: (cmd) => accent(cmd.name(), cmd.usage()),
+    argumentTerm: (arg) => accent(arg.name()),
+  })
+  .addHelpCommand(false)
+  .showHelpAfterError(true);
+
+
+/**
+ * * CLI Commands
+ */
+
+program
+  .command("crn")
+  .option(
+    "--dsn <dsn>",
+    "dsn of the canyon server"
+  )
+  .option(
+    "--reporter <reporter>",
+    "reporter auth token"
+  )
+  .option(
+    "--project_id <project_id>",
+    "id of the project"
+  )
+  .option("--commit_sha <commit_sha>", "commit sha of the project")
+  .option("--branch <branch>", "branch of the project")
+  .option("--buildId <buildId>", "buildId of the project")
+  .option("--report_id <report_id>", "report id of the case")
+  .option("--gitlab_url <gitlab_url>", "gitlab url of the project")
+  .option("--payload_path <payload_path>", "payload_path")
+  .option("--appid <appid>", "appid")
+  .option("--module <module>", "module")
+  .allowExcessArguments(false)
+  .allowUnknownOption(false)
+  .description("modify react native project code to adapt to canyon")
+  .addHelpText(
+    "after",
+    `\nFor help, head on to ${accent(
+      "https://github.com/canyon-project/canyon"
+    )}`
+  )
+  .action(async (params, options) => await crnAction(params, options));
+
 export const cli = async (args: string[]) => {
 	try {
-		console.log("???", chalk.greenBright("canyon-cli"));
-		// await program.parseAsync(args);
+		// console.log("???", chalk.greenBright("canyon-cli"));
+		await program.parseAsync(args);
 	} catch (e) {}
 };
 
