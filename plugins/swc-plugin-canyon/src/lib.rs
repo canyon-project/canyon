d74ece4ff35773f33de77aeada5e2c421f980be7
zhangtao25 <wr.zhang25@gmail.com>
2024-11-27 11:16:00 +0800

    chore: repo overall update

--- plugins/swc-plugin-canyon/src/lib.rs
+++ plugins/swc-plugin-canyon/src/lib.rs
@@ -1,26 +1,18 @@
+use swc_core::ecma::ast::{IdentName, PropOrSpread};
 use swc_core::ecma::{
     ast::{
-        Program, Module, Expr, Stmt, ModuleItem, Ident, Lit, KeyValueProp, Prop, PropName,
-        ObjectLit, ExprStmt, AssignExpr, AssignOp, MemberExpr, MemberProp,
+        Expr, KeyValueProp, ObjectLit, Program,
+        Prop,
+        PropName
     },
     transforms::testing::test_inline,
     visit::{as_folder, FoldWith, VisitMut},
 };
-use swc_core::ecma::ast::{AssignTarget};
-use swc_core::ecma::visit::VisitMutWith;
 use swc_core::plugin::{plugin_transform, proxies::TransformPluginProgramMetadata};
-use swc_core::plugin::metadata::TransformPluginMetadataContextKind;
 
-// Add this to the top of your file or a relevant module
+
 use serde::{Deserialize, Serialize};
-
-
-
-// https://github.com/swc-project/plugins/blob/main/packages/react-remove-properties/transform/src/lib.rs
-// 暂时方案，先一股脑把环境变量都注入，例如CI_COMMIT_BRANCH、CI_COMMIT_SHA、CI_PROJECT_ID等，
-// 可配置的是compareTarget
-
-
+use swc_core::plugin::metadata::TransformPluginMetadataContextKind;
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Config {
@@ -48,113 +40,95 @@ impl Default for Config {
 }
 
 pub struct TransformVisitor {
-    injected: bool,
     config: Config,
 }
-
 impl TransformVisitor {
     pub fn new() -> Self {
-        Self { injected: false,
-            config: Config::default() }
+        Self { config: Config::default() }
+    }
+    // 处理对象字面量属性的函数
+    fn process_coverage_data_object(&mut self, obj: &mut ObjectLit) {
+        let excluded_keys = ["statementMap", "fnMap", "branchMap", "inputSourceMap"];
+
+        // 过滤掉指定的属性
+        obj.props.retain(|prop| {
+            match prop {
+                PropOrSpread::Prop(prop) => {
+                    if let Prop::KeyValue(KeyValueProp {
+                                              key: PropName::Ident(IdentName { sym, .. }),
+                                              ..
+                                          }) = &**prop {
+                        // 排除指定的属性名
+                        !excluded_keys.contains(&sym.as_ref())
+                    } else {
+                        true
+                    }
+                }
+                _ => true,
+            }
+        });
     }
 }
 
 impl VisitMut for TransformVisitor {
-    fn visit_mut_module(&mut self, module: &mut Module) {
-        module.visit_mut_children_with(self);
-        if !self.injected {
-            let window_canyon = Expr::Member(MemberExpr {
-                obj: Box::new(Expr::Ident(Ident::new("(new Function('return this')())".into(), Default::default()))),
-                prop: MemberProp::Ident(Ident::from(Ident::new("__canyon__".into(), Default::default()))),
-                span: Default::default(),
-            });
-            // let dsn = std::env::var("DSN").unwrap_or("-".to_string());
-            // let reporter = std::env::var("REPORTER").unwrap_or("-".to_string());
-            // let instrumentCwd = std::env::current_dir().unwrap().to_str().unwrap_or("-").to_string();
-            // let branch = std::env::var("CI_COMMIT_BRANCH").unwrap_or("-".to_string());
-            // let sha = std::env::var("CI_COMMIT_SHA").unwrap_or("-".to_string());
-            // let projectID = std::env::var("CI_PROJECT_ID").unwrap_or("-".to_string());
+    // 遍历每个表达式时，修改表达式
+    fn visit_mut_expr(&mut self, expr: &mut Expr) {
+        match expr {
+            // 当表达式是对象字面量时
+            Expr::Object(ref mut obj) => {
+                // 调用 visit_mut_object_lit 来处理对象字面量的属性
+                self.visit_mut_object_lit(obj);
+            }
+            _ => {}
+        }
+    }
 
-            let dsn = self.config.dsn.clone().unwrap_or("-".to_string());
-            let reporter = self.config.reporter.clone().unwrap_or("-".to_string());
-            let instrumentCwd = self.config.instrumentCwd.clone().unwrap_or("-".to_string());
-            let branch = self.config.branch.clone().unwrap_or("-".to_string());
-            let sha = self.config.sha.clone().unwrap_or("-".to_string());
-            let projectID = self.config.projectID.clone().unwrap_or("-".to_string());
-            let compareTarget = self.config.compareTarget.clone().unwrap_or("-".to_string());
+    fn visit_mut_object_lit(&mut self, obj: &mut ObjectLit) {
+        // 定义一个字符串数组，包含需要排除的属性名
+        let excluded_keys = ["statementMap", "fnMap", "branchMap","inputSourceMap"];
+        let required_keys = ["statementMap", "fnMap", "branchMap"];
 
-            // 打印出这些
+        // 定义需要同时包含的属性
+        let required_keys = ["statementMap", "fnMap", "branchMap"];
 
-            // println!("dsn: {}", dsn);
-            // println!("reporter: {}", reporter);
-            // println!("instrumentCwd: {}", instrumentCwd);
-            // println!("branch: {}", branch);
-            // println!("sha: {}", sha);
-            // println!("projectID: {}", projectID);
+        let dsn = self.config.dsn.clone().unwrap_or("-".to_string());
+        let reporter = self.config.reporter.clone().unwrap_or("-".to_string());
+        let instrumentCwd = self.config.instrumentCwd.clone().unwrap_or("-".to_string());
+        let branch = self.config.branch.clone().unwrap_or("-".to_string());
+        let sha = self.config.sha.clone().unwrap_or("-".to_string());
+        let projectID = self.config.projectID.clone().unwrap_or("-".to_string());
+        let compareTarget = self.config.compareTarget.clone().unwrap_or("-".to_string());
 
-            let object_lit = Expr::Object(ObjectLit {
-                props: vec![
-                    Prop::KeyValue(KeyValueProp {
-                        key: PropName::Ident(Ident::from(Ident::new("dsn".into(), Default::default()))),
-                        value: Box::new(Expr::Lit(Lit::Str(dsn.into()))),
-                }).into(),
+        // 打印出这些
 
-                    Prop::KeyValue(KeyValueProp {
-                        key: PropName::Ident(Ident::from(Ident::new("reporter".into(), Default::default()))),
-                        value: Box::new(Expr::Lit(Lit::Str(reporter.into()))),
-                }).into(),
+        println!("dsn: {}", dsn);
+        println!("reporter: {}", reporter);
+        println!("instrumentCwd: {}", instrumentCwd);
+        println!("branch: {}", branch);
+        println!("sha: {}", sha);
+        println!("projectID: {}", projectID);
 
-                    Prop::KeyValue(KeyValueProp {
-                        key: PropName::Ident(Ident::from(Ident::new("instrumentCwd".into(), Default::default()))),
-                        value: Box::new(Expr::Lit(Lit::Str(instrumentCwd.into()))),
-                }).into(),
+        // 检查对象字面量是否同时包含这些属性
+        let contains_required_keys = required_keys.iter().all(|&key| {
+            obj.props.iter().any(|prop| {
+                if let PropOrSpread::Prop(ref prop) = prop {
+                    if let Prop::KeyValue(KeyValueProp {
+                                              key: PropName::Ident(IdentName { sym, .. }),
+                                              ..
+                                          }) = &**prop {
+                        return sym.as_ref() == key;
+                    }
+                }
+                false
+            })
+        });
 
-                    Prop::KeyValue(KeyValueProp {
-                        key: PropName::Ident(Ident::from(Ident::new("branch".into(), Default::default()))),
-                        value: Box::new(Expr::Lit(Lit::Str(branch.into()))),
-                }).into(),
-
-                    Prop::KeyValue(KeyValueProp {
-                        key: PropName::Ident(Ident::from(Ident::new("sha".into(), Default::default()))),
-                        value: Box::new(Expr::Lit(Lit::Str(sha.into()))),
-                }).into(),
-
-                    Prop::KeyValue(KeyValueProp {
-                        key: PropName::Ident(Ident::from(Ident::new("projectID".into(), Default::default()))),
-                        value: Box::new(Expr::Lit(Lit::Str(projectID.into()))),
-                }).into(),
-
-                    Prop::KeyValue(KeyValueProp {
-                        key: PropName::Ident(Ident::from(Ident::new("compareTarget".into(), Default::default()))),
-                        value: Box::new(Expr::Lit(Lit::Str(compareTarget.into()))),
-                    }).into(),
-                ],
-                span: Default::default(),
-            });
-
-            let assign_expr = AssignExpr {
-                left: AssignTarget::try_from(Box::new(window_canyon)).unwrap(), // 直接使用 Box<Expr>
-                op: AssignOp::Assign,
-                right: Box::new(object_lit),
-                span: Default::default(),
-            };
-
-            let assign_stmt = Stmt::Expr(ExprStmt {
-                expr: Box::new(Expr::Assign(assign_expr)),
-                span: Default::default(),
-            });
-
-            module.body.push(ModuleItem::Stmt(assign_stmt));
-            self.injected = true;
+        // 只有在同时包含所有指定属性时才进行处理
+        if contains_required_keys {
+            self.process_coverage_data_object(obj);
         }
     }
 }
-
-// https://github.com/swc-project/plugins/blob/main/packages/react-remove-properties/transform/src/lib.rs
-// 暂时方案，先一股脑把环境变量都注入，例如CI_COMMIT_BRANCH、CI_COMMIT_SHA、CI_PROJECT_ID等，
-// 可配置的是compareTarget
-
-
 
 #[plugin_transform]
 pub fn process_transform(program: Program, metadata: TransformPluginProgramMetadata) -> Program {
@@ -178,10 +152,10 @@ pub fn process_transform(program: Program, metadata: TransformPluginProgramMetad
     let env = metadata.get_context(&TransformPluginMetadataContextKind::Env).unwrap_or("-".to_string());
     let filename = metadata.get_context(&TransformPluginMetadataContextKind::Filename).unwrap_or("-".to_string());
     let cwd = metadata.get_context(&TransformPluginMetadataContextKind::Cwd).unwrap_or("-".to_string());
-    // println!("env: {}", env);
-    // println!("filename: {}", filename);
-    // println!("cwd: {}", cwd);
-    program.fold_with(&mut as_folder(TransformVisitor { injected: false, config }))
+    println!("env: {}", env);
+    println!("filename: {}", filename);
+    println!("cwd: {}", cwd);
+    program.fold_with(&mut as_folder(TransformVisitor { config }))
 }
 
 test_inline!(
@@ -189,16 +163,7 @@ test_inline!(
     |_| as_folder(TransformVisitor::new()),
     boo,
     // 输入代码
-    r#"console.log("transform");"#,
+    r#"const coverageData={fnMap:"nihao",statementMap:"",branchMap:"sss"};"#,
     // 经插件转换后的输出代码
-    r#"console.log("transform");
-(new Function('return this')()).__canyon__ = {
-    dsn: "-",
-    reporter: "-",
-    instrumentCwd: "/",
-    branch: "-",
-    sha: "-",
-    projectID: "-"
-};
-"#
+    r#"const coverageData={};"#
 );
