525b299a726a7741ac1fa411eaefad46d945c004
zhangtao25 <wr.zhang25@gmail.com>
2024-11-29 11:57:51 +0800

    chore: repo overall update

--- packages/canyon-platform/next.config.ts
+++ packages/canyon-platform/next.config.ts
@@ -1,19 +1,24 @@
 import type { NextConfig } from "next";
-
-// 引入上层目录的.env配置
 import { resolve } from "node:path";
 import { config } from "dotenv";
 config({ path: resolve(__dirname, "../../.env") });
 
 const nextConfig: NextConfig = {
-  /* config options here */
   typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
   },
-  // reactStrictMode: true,
+  experimental: {
+    swcPlugins:
+      process.env.NODE_ENV === "production"
+        ? [
+            ["swc-plugin-coverage-instrument", {}],
+            ["swc-plugin-canyon", {}],
+          ]
+        : [],
+  },
 };
 
 export default nextConfig;
