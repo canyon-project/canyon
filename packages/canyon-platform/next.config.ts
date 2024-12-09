0482369d29a90eab1ab5f60a8f2070404639ef22
zhangtao25 <wr.zhang25@gmail.com>
2024-12-09 20:15:52 +0800

    chore: daily development

--- packages/canyon-platform/next.config.ts
+++ packages/canyon-platform/next.config.ts
@@ -13,7 +13,22 @@ import prisma from "@/lib/prisma";
 
 // flag position, do not delete
 
-export default async function () {
+export default async function (phase: string) {
+  console.log(phase, "phase");
+  if (
+    true
+    // phase === "phase-production-server" ||
+    // phase === "phase-development-server"
+  ) {
+    const sysSetting = await prisma.sysSetting.findMany({
+      where: {},
+    });
+    for (let i = 0; i < sysSetting.length; i++) {
+      process.env[sysSetting[i].key] = sysSetting[i].value;
+    }
+  }
+  console.log(process.env);
+  // console.log(sysSetting);
   const nextConfig: NextConfig = {
     typescript: {
       ignoreBuildErrors: true,
