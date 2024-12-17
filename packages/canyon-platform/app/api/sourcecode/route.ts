c92a09c72cb8d6bebccc256f6101198e0b2e5aca
zhangtao25 <wr.zhang25@gmail.com>
2024-12-17 11:00:53 +0800

    chore: daily development

--- packages/canyon-platform/app/api/sourcecode/route.ts
+++ packages/canyon-platform/app/api/sourcecode/route.ts
@@ -7,24 +7,22 @@ export async function GET(request: NextRequest) {
   const projectID = searchParams.get("project_id");
   const sha = searchParams.get("sha");
   const filepath = searchParams.get("filepath");
-  // console.log(projectID, sha, filepath);
-  // 必须projectID、sha、filepath都存在
+
   if (!projectID || !sha || !filepath) {
     return Response.error();
   }
   const [provider, id, slug] = projectID.split("-");
-  // console.log('?????')
+
   const gitProvider = await prisma.gitProvider.findFirst({
     where: {
       id: provider,
     },
   });
-  // console.log(gitProvider,'gitProvider')
+
   if (gitProvider) {
     // TODO: 从gitlab获取文件内容，还去要判断provider type，例如github。
     // 判断provider类型，例如gitlab、github、gitea
 
-    // console.log(gitProvider.type,'gitProvider.type')
     switch (gitProvider.type) {
       case "gitlab":
         const { url: gitlabUrl, privateToken: gitlabPrivateToken } =
@@ -47,13 +45,20 @@ export async function GET(request: NextRequest) {
         const { url: githubUrl, privateToken: githubPrivateToken } =
           gitProvider;
         // 对于github，其认证一般是通过Authorization头部带上token，格式为"Bearer <token>"
-        console.log(
-          `${"https://api.github.com"}/repos/${id}/contents/${"packages/canyon-report/" + filepath}`,
-          sha,
-        );
+
+        const githubProject = await prisma.project.findFirst({
+          where: {
+            id: projectID,
+          },
+        });
+
+        if (!githubProject) {
+          return Response.error();
+        }
+
         const githubResponse = await axios
           .get(
-            `${"https://api.github.com"}/repos/${id}/contents/${"packages/canyon-report/" + filepath}`,
+            `${"https://api.github.com"}/repos/${githubProject.pathWithNamespace}/contents/${"packages/canyon-report/" + filepath}`,
             {
               // 注意这里params中ref要改为branch，因为github API此处参数名是branch（假设你传入的sha对应的是分支名情况）
               params: {
