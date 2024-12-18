6eda1cc2b8e30657fba7aca409e71686be48f4a7
zhangtao25 <wr.zhang25@gmail.com>
2024-12-18 14:19:23 +0800

    chore: daily development

--- scripts/check.mjs
+++ scripts/check.mjs
@@ -1,6 +1,6 @@
 import fs from 'fs/promises';
 import path from 'path';
-import { fileURLToPath } from 'url';
+import {fileURLToPath} from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -9,22 +9,22 @@ const sourceFilePath = path.join(__dirname, '../.env.example');
 const destinationFilePath = path.join(__dirname, '../.env');
 
 (async () => {
-  try {
-    await fs.access(destinationFilePath);
-    console.log('The .env file already exists');
-  } catch (err) {
     try {
-      await fs.copyFile(sourceFilePath, destinationFilePath);
+        await fs.access(destinationFilePath);
+        console.log('The .env file already exists');
+    } catch (err) {
+        try {
+            await fs.copyFile(sourceFilePath, destinationFilePath);
 
-      let envStr = ``
-      Object.entries(process.env).forEach(([key, value]) => {
-        envStr += `${key}=${value}\n`
-      });
-      await fs.writeFile(destinationFilePath, envStr);
+            let envStr = ``
+            Object.entries(process.env).forEach(([key, value]) => {
+                envStr += `${key}=${value}\n`
+            });
+            await fs.writeFile(destinationFilePath, envStr);
 
-      console.log('The .env file has been successfully created');
-    } catch (error) {
-      console.error('Error copying file:', error);
+            console.log('The .env file has been successfully created');
+        } catch (error) {
+            console.error('Error copying file:', error);
+        }
     }
-  }
 })();
--- scripts/prisma.mjs
+++ scripts/prisma.mjs
@@ -8,15 +8,15 @@ const targetDirPath = './packages/canyon-collect/prisma';
 
 // First, check whether the target directory exists. If it exists, delete it (to achieve the effect of forced overwrite).
 if (fs.existsSync(targetDirPath)) {
-  fse.removeSync(targetDirPath);
+    fse.removeSync(targetDirPath);
 }
 
 // Then copy the source directory to the target directory.
 fse.copySync(sourceDirPath, targetDirPath);
 
-console.log('The file directory has been successfully copied from', sourceDirPath,'to', targetDirPath);
+console.log('The file directory has been successfully copied from', sourceDirPath, 'to', targetDirPath);
 
 // Then copy the source directory to another target directory.
 fse.copySync(sourceDirPath, './packages/canyon-platform/prisma');
 
-console.log('The file directory has been successfully copied from', sourceDirPath,'to', targetDirPath);
+console.log('The file directory has been successfully copied from', sourceDirPath, 'to', targetDirPath);
