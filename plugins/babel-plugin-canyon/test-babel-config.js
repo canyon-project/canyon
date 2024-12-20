860c787b0b129b4f86e6579693ce604463f7ebf6
zhangtao25 <wr.zhang25@gmail.com>
2024-12-20 10:25:16 +0800

    feat: update

--- plugins/babel-plugin-canyon/test-babel-config.js
+++ plugins/babel-plugin-canyon/test-babel-config.js
@@ -1,6 +1,23 @@
 module.exports = {
   plugins: [
     'istanbul',
-    './lib'
+    ['./lib',{
+      // 流水线侦测配置
+      projectID:'9050',
+      sha:'xxxxx',
+      branch:'master',
+      // 流水线变量里配置
+      dsn:'http://localhost:3000',
+      reporter:'xxxxx',
+      // 插件里配置
+      compareTarget: 'main',
+      provider: 'tripgl',
+      // ==========以上是属性=============
+      // 代理配置
+      oneByOne: true, //可配置代理 默认false
+      special: true, //默认false
+      keepMap: true, // 默认false
+      addAttributes:['branch'], //默认全部，有的话用有的，适用于生产
+    }]
   ]
 }
