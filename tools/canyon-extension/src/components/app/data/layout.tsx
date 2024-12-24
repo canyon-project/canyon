2b4d94889e5fe54cb306cfecc7dd6d5d70758c49
Allen Zhang <37892968+zhangtao25@users.noreply.github.com>
2024-12-24 23:55:08 +0800

    chore: daily development

---
+++ tools/canyon-extension/src/components/app/data/layout.tsx
@@ -0,0 +1,20 @@
+import { Space, theme, Typography } from 'antd';
+
+const { useToken } = theme;
+const { Text } = Typography;
+const AppDataLayout = ({ label, value }: any) => {
+  const { token } = useToken();
+  return (
+    <div>
+      <Space>
+        <Space style={{ fontSize: '14px', color: token.colorTextSecondary }}>
+          {label}
+          <span>:</span>
+        </Space>
+        <Text>{value}</Text>
+      </Space>
+    </div>
+  );
+};
+
+export default AppDataLayout;
---
+++ tools/canyon-extension/src/main.tsx
@@ -0,0 +1,8 @@
+import './index.css';
+import 'antd/dist/reset.css';
+
+import ReactDOM from 'react-dom/client';
+
+import App from './App.tsx';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
