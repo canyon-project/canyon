c975b2c4b776574d461eba07b2658a70cbc9b7ae
zhangtao25 <wr.zhang25@gmail.com>
2024-12-26 15:42:14 +0800

    chore: daily development

--- packages/canyon-backend/src/cov/coverage.module.ts
+++ packages/canyon-backend/src/cov/coverage.module.ts
@@ -1,11 +1,17 @@
 import { Module } from "@nestjs/common";
 import { PrismaService } from "../prisma/prisma.service";
 import { CoverageController } from "./coverage.controller";
-import { CoverageService } from "./services/coverage.service";
+import { CoveragePreStoreService } from "./services/coverage-pre-store.service";
 import { TestExcludeService } from "./services/common/test-exclude.service";
+import { CoverageDataComputeService } from "./services/coverage-data-compute.service";
 
 @Module({
     controllers: [CoverageController],
-    providers: [PrismaService, CoverageService, TestExcludeService],
+    providers: [
+        PrismaService,
+        CoveragePreStoreService,
+        CoverageDataComputeService,
+        TestExcludeService,
+    ],
 })
 export class CoverageModule {}
---
+++ packages/canyon-backend/src/cov/services/coverage-data-compute.service.ts
@@ -0,0 +1,142 @@
+// 覆盖率实体数据计算，适用于reportID数组计算
+
+import { HttpException, Injectable } from "@nestjs/common";
+import { PrismaService } from "src/prisma/prisma.service";
+import {
+    resetCoverageDataMap,
+    reorganizeCompleteCoverageObjects,
+    CoverageSummaryDataMap,
+    mergeCoverageMap,
+    genSummaryMapByCoverageMap,
+} from "canyon-data";
+import { removeNullKeys } from "../../utils/utils";
+import { decompressedData } from "../../utils/zstd";
+import { convertDataFromCoverageMapDatabase } from "../../utils/coverage";
+import { remapCoverageWithInstrumentCwd } from "canyon-map";
+import { CoverageSummaryDto } from "../dto/coverage-summary.dto";
+import { CoverageMapDto } from "../dto/coverage-map.dto";
+import { CoverageMapData } from "istanbul-lib-coverage";
+import { TestExcludeService } from "./common/test-exclude.service";
+
+/**
+ * Service to handle pre-storage operations for code coverage data.
+ * This service provides methods for retrieving and processing coverage summary and coverage map data.
+ *
+ * It interacts with the Prisma service to fetch coverage-related data from the database, decompresses the
+ * data, processes it, and formats it before returning the results.
+ *
+ * Methods:
+ * - coverageSummaryMap: Retrieves and decompresses the coverage summary for a specific project and report.
+ * - coverageMap: Retrieves, processes, and reorganizes coverage map data for a specific project and report.
+ *
+ * Dependencies:
+ * - PrismaService: Provides access to the database to retrieve coverage and coverage map data.
+ * - canyon-data: Utilities for resetting, reorganizing, and manipulating coverage data.
+ * - canyon-map: Utility to remap coverage data based on the instrumented current working directory.
+ */
+@Injectable()
+export class CoverageDataComputeService {
+    constructor(
+        private readonly prisma: PrismaService,
+        private readonly testExcludeService: TestExcludeService,
+    ) {}
+
+    /**
+     * Retrieves the coverage summary map for a specific project, commit (SHA), and report ID.
+     *
+     * @param {CoverageSummaryDto} dto - The data transfer object containing project ID, SHA, and report ID.
+     * @returns {Promise<CoverageSummaryDataMap>} - A promise that resolves to the decompressed coverage summary map.
+     * @throws {HttpException} - If the coverage summary data is not found, an exception with status 404 is thrown.
+     */
+    async coverageSummaryMap({
+        projectID,
+        sha,
+        reportID,
+    }: CoverageSummaryDto): Promise<CoverageSummaryDataMap> {
+        const coverage = await this.coverageMap({
+            projectID,
+            sha,
+            reportID,
+            filepath: undefined,
+        });
+        const codechanges = await this.prisma.codechange.findMany({
+            where: {
+                projectID,
+                sha,
+            },
+        });
+        return genSummaryMapByCoverageMap(
+            await this.testExcludeService.invoke(projectID, coverage),
+            codechanges,
+        );
+    }
+
+    /**
+     * Retrieves and processes the coverage map data for a specific project, commit (SHA), report ID, and file path.
+     *
+     * @param {CoverageMapDto} dto - The data transfer object containing project ID, SHA, report ID, and file path.
+     * @returns {Promise<CoverageMapData>} - A promise that resolves to the processed and reorganized coverage map data.
+     */
+    async coverageMap({
+        projectID,
+        sha,
+        reportID,
+        filepath,
+    }: CoverageMapDto): Promise<CoverageMapData> {
+        const coverages = await this.prisma.coverage.findMany({
+            where: {
+                sha,
+                projectID,
+                reportID: {
+                    in: reportID.split(","),
+                },
+                covType: reportID ? "agg" : "all",
+            },
+        });
+
+        if (coverages.length === 0) {
+            throw new HttpException(
+                {
+                    statusCode: 404,
+                    message: "coverage data not found",
+                    errorCode: "COVERAGE_DATA_NOT_FOUND",
+                },
+                404,
+            );
+        }
+
+        // 异步解压
+        const hits = await Promise.all(
+            coverages.map((coverage) => {
+                return decompressedData<{
+                    [key: string]: object;
+                }>(coverage.hit);
+            }),
+        );
+
+        // 防止内存爆炸，逐一合并
+        let hit = {};
+
+        for (let i = 0; i < hits.length; i++) {
+            hit = mergeCoverageMap(hit, hits[i]);
+        }
+
+        const coverageMaps = await this.prisma.coverageMap.findMany({
+            where: removeNullKeys({
+                sha,
+                projectID,
+                path: filepath,
+            }),
+        });
+
+        const { map, instrumentCwd } =
+            await convertDataFromCoverageMapDatabase(coverageMaps);
+
+        const reMapMap = await remapCoverageWithInstrumentCwd(
+            resetCoverageDataMap(map),
+            instrumentCwd,
+        );
+
+        return reorganizeCompleteCoverageObjects(reMapMap, hit);
+    }
+}
