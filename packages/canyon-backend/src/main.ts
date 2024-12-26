def5b04dcd3636a48dba02f704a8058e4556fa1a
zhangtao25 <wr.zhang25@gmail.com>
2024-12-26 10:35:46 +0800

    chore: daily development

--- packages/canyon-backend/src/main.ts
+++ packages/canyon-backend/src/main.ts
@@ -1,9 +1,7 @@
 import { NestFactory } from "@nestjs/core";
-import { json } from "express";
 import { ValidationPipe } from "@nestjs/common";
 import * as dotenv from "dotenv";
 import * as path from "node:path";
-import { GlobalExceptionFilter } from "./filter/global-exception.filter";
 
 dotenv.config({
     path: path.resolve(__dirname, "../../../.env"),
@@ -13,14 +11,8 @@ async function bootstrap() {
     const { AppModule } = await import("./app.module");
     const app = await NestFactory.create(AppModule);
     app.useGlobalPipes(new ValidationPipe());
-    // 使用全局过滤器
-    app.useGlobalFilters(new GlobalExceptionFilter());
-    app.use(
-        json({
-            limit: "50mb",
-        }),
-    );
     app.enableCors();
     await app.listen(process.env["PORT"] || 8080);
 }
+
 bootstrap();
