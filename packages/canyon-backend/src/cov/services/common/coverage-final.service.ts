70fc7cf9ba5eb240536ce6ef6272360171b59f03
zhangtao25 <wr.zhang25@gmail.com>
2024-12-30 11:08:16 +0800

    feat: migrate projectID to repoID

--- packages/canyon-backend/src/cov/services/common/coverage-final.service.ts
+++ packages/canyon-backend/src/cov/services/common/coverage-final.service.ts
@@ -1,7 +1,5 @@
 import { Injectable } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
-// import { decompressedData } from "canyon-map";
-// import { convertDataFromCoverageMapDatabase } from "canyon-map";
 import {
     remapCoverageWithInstrumentCwd,
     convertDataFromCoverageMapDatabase,
@@ -35,6 +33,49 @@ export class CoverageFinalService {
         },
         hit?: { [key: string]: object },
     ) {
+        // 如果外部传入了hit，就不再从数据库中获取hit
+        hit =
+            hit ||
+            (await this.getHitByProjectIDShaReportID({
+                projectID,
+                sha,
+                reportID,
+            }));
+
+        // 无论是外部hit，还是查询到的hit，都为空对象，直接返回空对象
+        if (Object.keys(hit).length === 0) {
+            return {};
+        }
+
+        const coverageMaps = await this.prisma.coverageMap.findMany({
+            where: {
+                sha,
+                repoID: projectID.split("-")[1],
+                path: filepath,
+            },
+        });
+
+        // map没有也一样返回空对象
+        if (coverageMaps.length === 0) {
+            return {};
+        }
+
+        // 等会删掉
+        const { map, instrumentCwd } = await convertDataFromCoverageMapDatabase(
+            coverageMaps.map((i) => ({
+                ...i,
+                projectID: "11",
+            })),
+        );
+
+        const reMapMap = await remapCoverageWithInstrumentCwd(
+            resetCoverageDataMap(map),
+            instrumentCwd,
+        );
+        return reorganizeCompleteCoverageObjects(reMapMap, hit);
+    }
+
+    private async getHitByProjectIDShaReportID({ projectID, sha, reportID }) {
         const coverages = await this.prisma.coverage.findMany({
             where: {
                 sha,
@@ -55,37 +96,6 @@ export class CoverageFinalService {
                 await decompressedData(coverages[i].hit),
             );
         }
-
-        // 如果外部传入了hit，就不再从数据库中获取hit
-        hit = hit || hitBox;
-
-        // 无论是外部hit，还是查询到的hit，都为空对象，直接返回空对象
-        if (Object.keys(hit).length === 0) {
-            return {};
-        }
-
-        const coverageMaps = await this.prisma.coverageMap.findMany({
-            where: {
-                sha,
-                projectID,
-                path: filepath,
-            },
-        });
-
-        // map没有也一样返回空对象
-        if (coverageMaps.length === 0) {
-            return {};
-        }
-
-        const { map, instrumentCwd } =
-            await convertDataFromCoverageMapDatabase(coverageMaps);
-
-        const reMapMap = await remapCoverageWithInstrumentCwd(
-            resetCoverageDataMap(map),
-            instrumentCwd,
-        );
-
-        // console.log("reMapMap", reMapMap);
-        return reorganizeCompleteCoverageObjects(reMapMap, hit);
+        return hitBox;
     }
 }
