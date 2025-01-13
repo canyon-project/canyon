7c3c4735abb88ff8fb0f22f0d0200ea014e1805a
zhangtao25 <wr.zhang25@gmail.com>
2025-01-13 20:05:24 +0800

    chore: daily development

--- packages/canyon-ut/next.config.ts
+++ packages/canyon-ut/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 import { config } from "dotenv";
 import { resolve } from "node:path";
 config({ path: resolve(__dirname, "../../.env") });
+
+// flag position, do not delete
+
 const nextConfig: NextConfig = {
   /* config options here */
     typescript: {
