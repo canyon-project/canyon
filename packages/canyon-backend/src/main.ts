0a566521bbc476473c6cd01d42d806bed6b399ed
zhangtao25 <wr.zhang25@gmail.com>
2025-01-14 20:27:03 +0800

    chore: daily development

--- packages/canyon-backend/src/main.ts
+++ packages/canyon-backend/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from "@nestjs/core";
 import { ValidationPipe } from "@nestjs/common";
 import * as dotenv from "dotenv";
 import * as path from "node:path";
+import { json } from "express";
 
 dotenv.config({
   path: path.resolve(__dirname, "../../../.env"),
@@ -10,6 +11,11 @@ dotenv.config({
 async function bootstrap() {
   const { AppModule } = await import("./app.module");
   const app = await NestFactory.create(AppModule);
+  app.use(
+    json({
+      limit: "50mb",
+    }),
+  );
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
   await app.listen(process.env["PORT"] || 8080);
