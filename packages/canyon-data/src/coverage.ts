abd01c6a4f00d3dde5f029edb8e42a217fc68b86
zhangtao25 <wr.zhang25@gmail.com>
2025-01-14 20:56:49 +0800

    chore: daily development

--- packages/canyon-data/src/coverage.ts
+++ packages/canyon-data/src/coverage.ts
@@ -1,3 +1,25 @@
+function genHitByMap(mapValue:any) {
+  return {
+    s: Object.entries(mapValue.statementMap).reduce((accInside, [keyInside]) => {
+      // @ts-ignore
+      accInside[keyInside] = 0;
+      return accInside;
+    }, {}),
+    f: Object.entries(mapValue.fnMap).reduce((accInside, [keyInside]) => {
+      // @ts-ignore
+      accInside[keyInside] = 0;
+      return accInside;
+    }, {}),
+    b: Object.entries(mapValue.branchMap).reduce(
+      (accInside, [keyInside, valueInside]: any) => {
+        // @ts-ignore
+        accInside[keyInside] = Array(valueInside.length).fill(0);
+        return accInside;
+      },
+      {},
+    ),
+  }
+}
 export const reorganizeCompleteCoverageObjects = (
   map: {
     [key: string]: object;
@@ -9,11 +31,18 @@ export const reorganizeCompleteCoverageObjects = (
   // 重组的时候以map为基准，map可能是只查询一部分。
   const obj = {};
   for (const mapKey in map) {
-    if (map[mapKey] && hit[mapKey]) {
+    if (hit[mapKey]) {
       // @ts-ignore
       obj[mapKey] = {
         ...map[mapKey],
         ...hit[mapKey],
+        path: mapKey,
+      }
+    } else {
+      // @ts-ignore
+      obj[mapKey] = {
+        ...map[mapKey],
+        ...genHitByMap(map[mapKey]),
         path: mapKey,
       }
     }
