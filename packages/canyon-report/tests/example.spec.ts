d5f1bc9da82adc9716c65459929a3cc27629975c
zhangtao25 <wr.zhang25@gmail.com>
2025-01-14 15:48:52 +0800

    chore: daily development

--- packages/canyon-report/tests/example.spec.ts
+++
@@ -1,6 +0,0 @@
-import { test, expect } from './baseFixtures';
-
-test('has title', async ({ page }) => {
-  await page.goto('https://report-component.canyonjs.org');
-  expect(await page.title()).toBe('Rsbuild App');
-});
