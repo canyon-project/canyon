b836d80c1dd50511bbec343ca2bae87754c0e050
zhangtao25 <wr.zhang25@gmail.com>
2025-01-15 17:18:19 +0800

    chore: daily development

--- packages/canyon-backend/src/app.controller.ts
+++ packages/canyon-backend/src/app.controller.ts
@@ -5,8 +5,12 @@ import { convertSystemSettingsFromTheDatabase } from "./utils/sys";
 @Controller()
 export class AppController {
   constructor(private readonly prisma: PrismaService) {}
-  @Get("/api/vi/health")
+  @Get("api/vi/health")
   async viHealth() {
+    return "230614";
+  }
+  @Get("vi/health")
+  async viHealth2() {
     return "230614";
   }
 
