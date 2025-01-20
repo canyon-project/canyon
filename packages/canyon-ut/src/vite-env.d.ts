2405957809adf7aa1a725b3c1c8d1af0645d32d6
wr.zhang25 <wr.zhang25@gmail.com>
2025-01-20 23:14:08 +0800

    chore: daily development

---
+++ packages/canyon-ut/src/vite-env.d.ts
@@ -0,0 +1,3 @@
+/// <reference types="vite/client" />
+// vite-env.d.ts
+/// <reference types="vite-plugin-pages/client-react" />
---
+++ packages/canyon-ut/tailwind.config.ts
@@ -0,0 +1,19 @@
+import type { Config } from "tailwindcss";
+
+const config = {
+  darkMode: "class",
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "../canyon-report/src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  corePlugins: {
+    preflight: false,
+  },
+} satisfies Config;
+
+export default config;
