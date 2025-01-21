7d728cfab5cc5617a3f164bd61a12717a41acc55
zhangtao25 <wr.zhang25@gmail.com>
2025-01-21 19:11:31 +0800

    chore: daily development

--- packages/canyon-ut/server.js
+++ packages/canyon-ut/server.js
@@ -24,7 +24,7 @@ const app = express();
 
 app.use(cors());
 app.use(history()); // 这里千万要注意，要在static静态资源上面
-app.use(express.static("dist"));
+app.use(express.static(path.join(__dirname, 'dist')));
 app.use(express.json());
 
 
