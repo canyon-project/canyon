3bb1b9575b3831503ddb4bc81becbfb3babbaa19
Allen Zhang <zhangtao25@Allens-MacBook-Pro.local>
2025-01-31 13:51:59 +0800

    chore: daily development

--- examples/canyon-reporter-vitest/sum.test.js
+++ examples/canyon-reporter-vitest/sum.test.js
@@ -1,11 +1,6 @@
 import { expect, test } from 'vitest'
 import { sum } from './sum.js'
-import { ride } from './ride.js'
 
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3)
-})
-
-test('ride 1 * 2 to equal 2', () => {
-  expect(ride(1, 2)).toBe(2)
 })
