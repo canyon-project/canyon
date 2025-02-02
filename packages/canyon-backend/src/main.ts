8bd48d4540817724c0b1965ff1258f288fbc55af
zhangtao25 <wr.zhang25@gmail.com>
2025-02-02 18:16:16 +0800

    chore: daily development

--- packages/canyon-backend/src/main.ts
+++ packages/canyon-backend/src/main.ts
@@ -3,7 +3,7 @@ import { ValidationPipe } from "@nestjs/common";
 import * as dotenv from "dotenv";
 import * as path from "node:path";
 import { json } from "express";
-import * as history from "connect-history-api-fallback";
+
 dotenv.config({
   path: path.resolve(__dirname, "../../../.env"),
 });
@@ -12,7 +12,6 @@ async function bootstrap() {
   const { AppModule } = await import("./app.module");
   const app = await NestFactory.create(AppModule);
   // 使用 history API fallback 解决前端 history 模式 404 问题
-  app.use(history());
   app.use(
     json({
       limit: "50mb",
