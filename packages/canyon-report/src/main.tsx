8742188b0d2c44d38ada57c49af091eaf4840b4a
zhangtao25 <wr.zhang25@gmail.com>
2025-02-02 14:20:49 +0800

    chore: daily development

--- packages/canyon-report/src/main.tsx
+++ packages/canyon-report/src/main.tsx
@@ -1,10 +1,5 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
 import "./index.css";
+import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
+createRoot(document.getElementById("root")!).render(<App />);
