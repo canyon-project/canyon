412d4feb46ff506b87e16090b3efe0cd3ef36ebb
zhangtao25 <wr.zhang25@gmail.com>
2025-02-03 13:22:16 +0800

    chore: daily development

--- packages/canyon-platform/src/components/app/CopyCode.tsx
+++ packages/canyon-platform/src/components/app/CopyCode.tsx
@@ -4,25 +4,26 @@ import { CopyOutlined } from "@ant-design/icons";
 import { FC, useEffect } from "react";
 // @ts-ignore
 import { CopyToClipboard } from "react-copy-to-clipboard";
+// @ts-ignore
+import { codeToHtml } from "https://esm.sh/shiki@1.0.0";
 const CopyCode: FC<{ code: string }> = ({ code }) => {
   const fileContent = code;
   const [content, setContent] = useState("");
 
   useEffect(() => {
     if (fileContent) {
-      // createHighlighterCoreInstance().then(({ codeToHtml }) => {
-      //   const html = codeToHtml(fileContent, {
-      //     lang: "json",
-      //     theme: "tokyo-night",
-      //   });
-      //   setContent(html);
-      // });
+      codeToHtml(fileContent, {
+        lang: "json",
+        theme: "tokyo-night",
+      }).then((html) => {
+        setContent(html);
+      });
     }
   }, [fileContent]);
   return (
     <div className={"relative copy-code"}>
       <div className={"absolute right-[10px] top-[10px]"}>
-        <CopyToClipboard text={code}>
+        <CopyToClipboard text={code} onCopy={() => message.success("Copied!")}>
           <Button
             type={"link"}
             className={"btn hidden"}
