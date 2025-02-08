bb3dd579153a14db8cedeb596f4647618cce49b0
zhangtao25 <wr.zhang25@gmail.com>
2025-02-08 23:53:10 +0800

    chore: daily development

---
+++ release.mjs
@@ -0,0 +1,19 @@
+import { exec } from 'child_process';
+
+// 接收命令行参数
+
+const args = process.argv.slice(2);
+const [version] = args;
+
+// 执行命令
+
+console.log('version:', version);
+
+exec(`git tag ${version} ; git push origin ${version}`, (err, stdout, stderr) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(stdout);
+  console.log(stderr);
+});
