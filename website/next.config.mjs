f1fec362b3766c678965db58d226a7f298873c34
zhangtao25 <wr.zhang25@gmail.com>
2025-02-25 23:32:53 +0800

    chore: daily development

---
+++ website/next.config.mjs
@@ -0,0 +1,16 @@
+import nextra from "nextra";
+
+const withNextra = nextra({
+  // ... Other Nextra config options
+});
+
+// You can include other Next.js configuration options here, in addition to Nextra settings:
+export default withNextra({
+  // ... Other Next.js config options
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+});
