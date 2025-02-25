544e6a2f5ec68533ca85d26a04e25395f8a2fe91
zhangtao25 <wr.zhang25@gmail.com>
2025-02-25 23:32:53 +0800

    chore: daily development

---
+++ website/app/_meta.js
@@ -0,0 +1,29 @@
+export default {
+  index: {
+    type: "page",
+    display: "hidden",
+    title: "Introduction",
+    theme: {
+      // sidebar: true,
+      toc: false,
+    },
+  },
+  documentation: {
+    title: "文档",
+    type: "page",
+  },
+  support: {
+    title: "支持",
+    type: "page",
+  },
+  // company: {
+  //   title: "语言",
+  //   type: "menu",
+  //   items: {
+  //     en: {
+  //       title: "English",
+  //       href: "https://docs.canyonjs.org",
+  //     },
+  //   },
+  // },
+};
