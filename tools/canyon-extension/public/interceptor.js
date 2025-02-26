ece1e420f5bf25fbea743cd503fb5d9bfe753e24
zhangtao25 <wr.zhang25@gmail.com>
2025-02-26 15:32:39 +0800

    feat: const num = 6

--- tools/canyon-extension/public/interceptor.js
+++ tools/canyon-extension/public/interceptor.js
@@ -33,9 +33,9 @@ window.addEventListener('message', function (e) {
 setTimeout(()=>{
   // 先尝试本地获取，如果没有再去获取window上的数据
   const __canyon__interval__time__ = Number(localStorage.getItem('__canyon__interval__time__')) || Number(window?.__canyon__?.intervalTime);
-  if (!isNaN(__canyon__interval__time__)) {
+  if (true) {
     // const num = __canyon__interval__time__;
-    const num = 6;
+    const num = 10;
     if (num > 0) {
       setInterval(() => {
         if ((window.__canyon__||(Object.keys(window.__coverage__||{}).length>0 ? Object.values(window.__coverage__)[0] : undefined)) && window.__coverage__) {
