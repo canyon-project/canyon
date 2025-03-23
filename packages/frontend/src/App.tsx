d9925e695d91fd5d8047f6d4dcf5813718826e28
zhangtao25 <wr.zhang25@gmail.com>
2025-03-24 00:58:44 +0800

    feat: integrate canyon-report and remove unused styles

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,14 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
+import Report from "canyon-report";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <div>
-      s
+      <Report onSelect={(v)=>{
+        console.log(v)
+        return new Promise((resolve)=>{
+          setTimeout(()=> {
+            resolve({
+              fileCodeChange:[],
+              fileContent:'',
+              fileCoverage: {
+                path: '',
+                statementMap: {},
+                fnMap: {},
+                branchMap: {},
+                s: {},
+                f: {},
+                b: {}
+              }
+            })
+          })
+        })
+      }} theme={'light'} language={'ja'} name={'test'} dataSource={{}} value={''} defaultOnlyShowChanged={false} />
     </div>
   )
 }
