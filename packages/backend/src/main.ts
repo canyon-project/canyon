285e8bf6dde6a1891d102680690b4d8fe5a6264e
zhangtao25 <wr.zhang25@gmail.com>
2025-03-29 23:09:23 +0800

    chore: daily development

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -7,10 +7,6 @@ dotenv.config({
   path: path.resolve(__dirname, '../../../.env'),
 });
 
-process.env.DATABASE_URL =process.env.PAAS_ENV? atob(
-  `cG9zdGdyZXM6Ly91YXBwX2NhbnlvbjpPVGswT0RrMll6QGNhbnlvbi5wZy5kYi51YXQucWEubnQuY3RyaXBjb3JwLmNvbTo1NTc3Ny9jYW55b25kYj9zY2hlbWE9cHVibGlj`,
-):'postgres://postgres:123456@localhost:5432/postgres';
-
 async function bootstrap() {
   const { AppModule } = await import('./app.module');
   const app = await NestFactory.create(AppModule);
