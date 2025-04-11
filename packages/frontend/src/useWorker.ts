636bcea4cc97899295d22f7b747d9fa47472cf9e
zhangtao25 <wr.zhang25@gmail.com>
2025-04-11 14:21:58 +0800

    chore: daily development

--- packages/frontend/src/useWorker.ts
+++ packages/frontend/src/useWorker.ts
@@ -1,18 +1,18 @@
 import { loader } from "@monaco-editor/react";
 
-const UNPKG_URL = await fetch(`/api/base`)
-  .then((r) => {
-    return r.json();
-  })
-  .then((r) => r.UNPKG_URL || "https://unpkg.com");
-
+// const UNPKG_URL = await fetch(`/api/base`)
+//   .then((r) => {
+//     return r.json();
+//   })
+//   .then((r) => r.UNPKG_URL || "https://unpkg.com");
+const UNPKG_URL = "//unpkg.com";
 console.log(UNPKG_URL, "UNPKG_URL");
 loader.config({
   paths: { vs: `${UNPKG_URL}/monaco-editor@0.52.2/min/vs` },
 });
 
 loader.init().then((monaco) => {
-  import("monaco-themes/themes/Night Owl.json").then((data: any) => {
+  import("monaco-themes/themes/Night Owl.json").then((data:any) => {
     monaco.editor.defineTheme("nightOwl", data);
   });
 });
