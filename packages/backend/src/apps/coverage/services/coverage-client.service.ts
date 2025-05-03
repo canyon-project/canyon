78447fb67e27f656c23469fdc45d9e9930d5af5a
zhangtao25 <wr.zhang25@gmail.com>
2025-05-03 21:27:25 +0800

    chore: daily development

--- packages/backend/src/apps/coverage/services/coverage-client.service.ts
+++ packages/backend/src/apps/coverage/services/coverage-client.service.ts
@@ -21,6 +21,28 @@ import {
 import { transFormHash } from '../../../utils/hash';
 import { checkCoverageType } from '../../../utils/checkCoverageType';
 
+// 整体思路改动！！！！！
+// 整体思路改动！！！！！
+// 整体思路改动！！！！！
+// 整体思路改动！！！！！
+// 整体思路改动！！！！！
+// 整体思路改动！！！！！
+// 整体思路改动！！！！！
+
+// 先remap成规整的 ！
+
+// 全部组合成最终形态
+
+// const data = {
+//   relativePath: '/coverage',
+//   statementMap: {},
+//   noTransformStatementMap: {},
+//   s: {},
+//   f: {},
+// };
+
+// 分组，db查询
+
 // 此代码重中之重、核心中的核心！！！
 @Injectable()
 export class CoverageClientService {
@@ -64,6 +86,7 @@ export class CoverageClientService {
       },
     });
 
+    // review过了
     if (coverageType === 'hit' && findCoverage === null) {
       return {
         type: coverageType, // hit or map
@@ -71,13 +94,13 @@ export class CoverageClientService {
       };
     }
 
-    const isHas = await this.prisma.coverageMapRelation.count({
-      where: {
-        coverageID: coverageID,
-      },
-    });
-
     if (coverageType === 'hit') {
+      // 很有必要，因为绝大部分都是没有inputSourceMap的，90%以上，所以多一个inputSourceMap的标志，可以通过fiter判断是否要反查询，可以大大减少查询的数量
+      // TODO 暂定，字段名就叫 inputSourceMap 当是hit的时候，跟着bfs一起传，有才传，即1
+      // 如果有hit的话我肯定得查。
+      // this.prisma.coverageMapRelation.findMany();
+      // this.prisma.coverageSourceMap.findMany();
+      // 组合出原始map
       // 有findCoverage，基本上肯定是有coverageMapRelation了
       // if (isHas > 0) {
       //   // 如果sourceMapID有，那就是最复杂的情况
@@ -86,8 +109,38 @@ export class CoverageClientService {
       //   // 说明没有sourceMap，要报错，不能用
       //   console.log('没有sourceMap');
       // }
+
+      // 组合
+      // this.prisma.coverageMapRelation.findMany();
+      // this.prisma.coverageSourceMap.findMany();
+
+      // const genCov = fn();
+      //
+      // this.insertCoverageIntoDatabase(genCov);
+
+      if (
+        Object.values(coverage as CoverageQueryParams).filter(
+          (i) => i.inputSourceMap,
+        ).length > 0
+      ) {
+        console.log('????');
+        //   reMap后慢慢处理
+      } else {
+        //   直接插入hit了这边，不需要其他操作了应该？
+        const coverageHitInsertResult = await this.coverageHitInsertResult(
+          coverageID,
+          coverage,
+        );
+        return {
+          type: coverageType, // hit or map
+          coverageTable: findCoverage,
+          coverageHitInsertResult: coverageHitInsertResult,
+        };
+      }
     } else {
       //   如果是map的话，那逻辑就一样了
+
+      // 不管怎么样，先插入
       const coverageID = generateCoverageId({
         provider,
         sha,
@@ -120,11 +173,171 @@ export class CoverageClientService {
         .catch(() => {
           // console.log(r)
         });
+
+      //   检查要不要reMap
+      if (
+        Object.values(coverage as CoverageQueryParams).filter(
+          (i) => i.inputSourceMap,
+        ).length > 0
+      ) {
+        console.log('需要reMap');
+        // 还原覆盖率
+        const remapCoverageObject = await remapCoverageByOld(coverage);
+
+        const mapList = this.genMapList(remapCoverageObject);
+        const re1 = await this.coverageMapInsertResult(mapList);
+        // 封装**
+        const re2 = await this.coverageHitInsertResult(
+          coverageID,
+          remapCoverageObject,
+        );
+
+        return {
+          re1,
+          re2,
+        };
+      } else {
+        const mapList = this.genMapList(coverage);
+        await this.coverageMapInsertResult(mapList);
+        // 封装**
+        await this.coverageHitInsertResult(coverageID, coverage);
+      }
     }
+
+    // await this.insertCoverageIntoDatabase();
+
     return {
       type: coverageType, // hit or map
       coverageTable: findCoverage,
-      isHas,
     };
+  }
+  async insertCoverageIntoDatabase() {
+    // this.clickhouseClient
+    // this.prisma.coverageMapRelation
+    // this.prisma.coverageSourceMap
+
+    return {};
+  }
+
+  async coverageHitInsertResult(coverageID: string, coverage) {
+    return this.clickhouseClient.insert({
+      table: 'coverage_hit',
+      values: Object.values(coverage as CoverageQueryParams).map(
+        ({ s, path, f, b }) => {
+          return {
+            ts: Math.floor(new Date().getTime() / 1000),
+            coverage_id: coverageID, // 这里的hash_id是 coverageID，保证reportID维度的不重复
+            relative_path: path,
+            s: s,
+            f: f,
+            b: flattenBranchMap(b),
+          };
+        },
+      ),
+      format: 'JSONEachRow',
+    });
+  }
+
+  async coverageMapInsertResult(mapList) {
+    //   才需要插入map表
+    return this.clickhouseClient.insert({
+      table: 'coverage_map',
+      values: mapList.map((m) => ({
+        ts: Math.floor(new Date().getTime() / 1000),
+        hash: m.file_coverage_map_hash,
+        // input_source_map: m.input_source_map,
+        statement_map: m.statement_map,
+        fn_map: m.fn_map,
+        branch_map: m.branch_map,
+      })),
+      format: 'JSONEachRow',
+    });
+  }
+
+  genMapList(coverage) {
+    const mapList = Object.values(coverage as CoverageQueryParams)
+      .filter(
+        ({ statementMap, branchMap, fnMap }) =>
+          statementMap && branchMap && fnMap,
+      )
+      .map(({ statementMap, branchMap, fnMap, inputSourceMap, path }) => {
+        const source_map_hash_id = inputSourceMap
+          ? createHash('sha256')
+              .update(JSON.stringify(inputSourceMap))
+              .digest('hex')
+          : '';
+
+        const mapItem = {
+          relative_path: path,
+          source_map_hash_id: source_map_hash_id,
+          source_map: JSON.stringify(inputSourceMap),
+          statement_map: Object.fromEntries(
+            Object.entries(statementMap).map(([k, v]) => [
+              Number(k),
+              [v.start.line, v.start.column, v.end.line, v.end.column],
+            ]),
+          ),
+          fn_map: Object.fromEntries(
+            Object.entries(fnMap).map(([k, v]) => [
+              Number(k),
+              [
+                v.name,
+                v.line,
+                [
+                  v.decl.start.line,
+                  v.decl.start.column,
+                  v.decl.end.line,
+                  v.decl.end.column,
+                ],
+                [
+                  v.loc.start.line,
+                  v.loc.start.column,
+                  v.loc.end.line,
+                  v.loc.end.column,
+                ],
+              ],
+            ]),
+          ),
+          branch_map: Object.fromEntries(
+            Object.entries(branchMap).map(([k, v]) => [
+              Number(k),
+              [
+                getBranchTypeIndex(v.type),
+                v.line,
+                [
+                  v.loc.start.line,
+                  v.loc.start.column,
+                  v.loc.end.line,
+                  v.loc.end.column,
+                ],
+                v.locations.map((loc) => [
+                  loc.start.line,
+                  loc.start.column,
+                  loc.end.line,
+                  loc.end.column,
+                ]),
+              ],
+            ]),
+          ),
+        };
+
+        const file_coverage_map_hash = createHash('sha256')
+          .update(
+            JSON.stringify({
+              // input_source_map: mapItem.input_source_map, 把input_source_map移到relation表
+              statement_map: mapItem.statement_map,
+              fn_map: mapItem.fn_map,
+              branch_map: mapItem.branch_map,
+            }),
+          )
+          .digest('hex');
+
+        return {
+          ...mapItem,
+          file_coverage_map_hash, // 增加hash字段
+        };
+      });
+
+    return mapList;
   }
 }
