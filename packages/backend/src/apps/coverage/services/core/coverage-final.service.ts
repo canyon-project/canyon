06656d5b2a28c8d0f27673195bbd367e52125df4
zhangtao25 <wr.zhang25@gmail.com>
2025-05-07 16:51:22 +0800

    chore: daily development

--- packages/backend/src/apps/coverage/services/core/coverage-final.service.ts
+++ packages/backend/src/apps/coverage/services/core/coverage-final.service.ts
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import { Inject, Injectable } from '@nestjs/common';
 import { ClickHouseClient } from '@clickhouse/client';
 import { PrismaService } from '../../../../prisma/prisma.service';
@@ -157,23 +158,63 @@ export class CoverageFinalService {
       };
 
       const initCov = genHitByMap(fileCoverageItem);
+      const find = coverageHitQuerySqlResultJson
+        .filter((i) => i.fullFilePath === item.fullFilePath)
+        .reduce(
+          (previousValue, currentValue, currentIndex, array) => {
+            const { s: s1, f: f1, b: b1 } = currentValue;
+            const { s: s2, f: f2, b: b2 } = previousValue;
+            const s = [[], []];
 
-      const find = coverageHitQuerySqlResultJson.find(
-        (i) => i.fullFilePath === item.fullFilePath,
-      );
+            s[0] = [...new Set([...s1[0], ...s2[0]])].sort();
+
+            s[0].forEach((key) => {
+              s[1][key] = Number(s1[1][key] || 0) + Number(s2[1][key] || 0);
+            });
+
+            const f = [[], []];
+
+            f[0] = [...new Set([...f1[0], ...f2[0]])].sort();
+
+            f[0].forEach((key) => {
+              f[1][key] = Number(f1[1][key] || 0) + Number(f2[1][key] || 0);
+            });
+
+            const b = [[], []];
+
+            b[0] = [...new Set([...b1[0], ...b2[0]])].sort();
+
+            b[0].forEach((key) => {
+              b[1][key] = Number(b1[1][key] || 0) + Number(b2[1][key] || 0);
+            });
+
+            return {
+              ...previousValue,
+              s: s,
+              f: f,
+              b: b,
+            };
+          },
+          {
+            fullFilePath: item.fullFilePath,
+            b: [[], []],
+            f: [[], []],
+            s: [[], []],
+          },
+        );
 
       if (find) {
         const { s: merged_s, f: merged_f, b: merged_b } = find;
 
-        merged_s[0].forEach((j, jindex) => {
+        merged_s[0].forEach((j: any, jindex) => {
           initCov.s[j] = Number(merged_s[1][jindex]);
         });
 
-        merged_f[0].forEach((j, jindex) => {
+        merged_f[0].forEach((j: any, jindex) => {
           initCov.f[j] = Number(merged_f[1][jindex]);
         });
 
-        merged_b[0].forEach((j, jindex) => {
+        merged_b[0].forEach((j: any, jindex) => {
           const realB = decodeKey(j);
           const [a, b] = realB;
           initCov.b[a][b] = Number(merged_b[1][jindex]);
