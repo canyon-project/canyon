1c5d5292d1a45f4dab64e30ca920c9189f703060
zhangtao25 <wr.zhang25@gmail.com>
2025-05-08 15:35:01 +0800

    chore: daily development

--- packages/backend/src/apps/project/project.module.ts
+++ packages/backend/src/apps/project/project.module.ts
@@ -1,15 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { ProjectController } from './project.controller';
-import {GetRepoCommitsByRepoIdServices} from "./services/get-repo-commits-by-repo-id.services";
-import {GetRepoCommitByCommitShaServices} from "./services/get-repo-commit-by-commit-sha.services";
+import { GetRepoCommitsByRepoIdServices } from './services/get-repo-commits-by-repo-id.services';
+import { GetRepoCommitByCommitShaServices } from './services/get-repo-commit-by-commit-sha.services';
 @Module({
   imports: [PrismaModule],
   controllers: [ProjectController],
-  providers: [
-    GetRepoCommitsByRepoIdServices,
-    GetRepoCommitByCommitShaServices
-  ],
+  providers: [GetRepoCommitsByRepoIdServices, GetRepoCommitByCommitShaServices],
   exports: [],
 })
 export class ProjectModule {}
