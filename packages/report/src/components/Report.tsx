b7ab038d30c9a668bb5052f84f5d20d19975fd05
zhangtao25 <wr.zhang25@gmail.com>
2025-05-09 17:48:50 +0800

    chore: daily development

---
+++ packages/report/src/components/Report.tsx
@@ -0,0 +1,14 @@
+import { FC } from 'react';
+import { ReportProps } from '../types.ts';
+
+const ReportComponent: FC<ReportProps> = ({
+  theme,
+  onSelect,
+  value,
+  dataSource,
+  name,
+}) => {
+  return <div>ReportComponent</div>;
+};
+
+export default ReportComponent;
