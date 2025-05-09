c0093a8644543aed6abce9fe4480801e90588fc6
zhangtao25 <wr.zhang25@gmail.com>
2025-05-09 17:49:36 +0800

    chore: daily development

--- packages/report/src/components/helpers/color.ts
+++
@@ -1,9 +0,0 @@
-export const getColor = (pct: number) => {
-  if (pct >= 80) {
-    return "rgb(33,181,119)";
-  } else if (pct >= 50) {
-    return "rgb(244,176,27)";
-  } else {
-    return "rgb(245,32,32)";
-  }
-};
