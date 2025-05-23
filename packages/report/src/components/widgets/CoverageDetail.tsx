8ac8ffefc36a1e2a3f3dd486da741658bb01fcc9
zhangtao25 <wr.zhang25@gmail.com>
2025-05-23 23:26:59 +0800

    chore: daily development

--- packages/report/src/components/widgets/CoverageDetail.tsx
+++ packages/report/src/components/widgets/CoverageDetail.tsx
@@ -9,40 +9,15 @@ const FileCoverageDetail: FC<{
   fileCoverage: any;
 }> = ({ fileContent, fileCoverage, fileCodeChange }) => {
   useEffect(() => {
-    // initCanyonSpa(document.getElementById('box'), {
-    //   coverage: fileCoverage,
-    //   content: fileContent,
-    //   diff:fileCodeChange
-    // });
-
-    // 创建 script 元素
-    const script = document.createElement('script');
-    // @ts-ignore
-    script.src = (window.UNPKG_URL || 'https://unpkg.com') +
-      '/canyon-report-spa/dist/index.umd.cjs'; // 替换为实际 URL
-    // script.async = true; // 异步加载（默认）
-
-    // 设置加载成功和失败的回调
-    script.onload = () => {
-      console.log('脚本加载成功');
-      // 这里可以执行依赖该脚本的代码
-      // @ts-ignore
-      window.CanyonReportSpa.initCanyonSpa(
-        document.getElementById('canyon-report-box'),
-        {
-          coverage: fileCoverage,
-          content: fileContent,
-          diff: fileCodeChange,
-        },
-      );
-    };
-
-    script.onerror = () => {
-      console.error('脚本加载失败');
-    };
-
-    // 将脚本添加到页面
-    document.head.appendChild(script);
+    import(
+      `${window.UNPKG_URL || 'https://unpkg.com'}/canyon-report-spa/dist/index.js`
+    ).then((res) => {
+      res.initCanyonSpa(document.getElementById('canyon-report-box'), {
+        coverage: fileCoverage,
+        content: fileContent,
+        diff: fileCodeChange,
+      });
+    });
   }, []);
 
   return (
