767af275c92def9b108e15b51f669b512c78f504
zhangtao25 <wr.zhang25@gmail.com>
2025-05-23 18:38:17 +0800

    chore: daily development

--- packages/report/src/index.tsx
+++ packages/report/src/index.tsx
@@ -1,12 +1,12 @@
-import { FC, useEffect } from "react";
-import { ConfigProvider, theme } from "antd";
-import enUS from "antd/es/locale/en_US";
-import jaJP from "antd/es/locale/ja_JP";
-import zhCN from "antd/es/locale/zh_CN";
-import { LanguageProvider } from "./locales";
-import ReportComponent from "./components/Report";
-import { ReportProps } from "./types";
-import { emptyFileCoverageData } from "./components/helpers/const";
+import { FC, useEffect } from 'react';
+import { ConfigProvider, theme } from 'antd';
+import enUS from 'antd/es/locale/en_US';
+import jaJP from 'antd/es/locale/ja_JP';
+import zhCN from 'antd/es/locale/zh_CN';
+import { LanguageProvider } from './locales';
+import ReportComponent from './components/Report';
+import { ReportProps } from './types';
+import { emptyFileCoverageData } from './components/helpers/const';
 
 const languages = {
   cn: zhCN,
@@ -18,7 +18,7 @@ const { darkAlgorithm } = theme;
 
 const onSelectDefault = () => {
   return Promise.resolve({
-    fileContent: "",
+    fileContent: '',
     fileCoverage: emptyFileCoverageData,
     fileCodeChange: [],
   });
@@ -26,15 +26,15 @@ const onSelectDefault = () => {
 
 const Report: FC<ReportProps> = (props) => {
   const {
-    theme = "light",
-    language = "en",
+    theme = 'light',
+    language = 'en',
     dataSource = {},
     onSelect = onSelectDefault,
     defaultOnlyShowChanged = false,
-    value = "",
-    name = "untitled",
+    value = '',
+    name = 'untitled',
   } = props;
-  const isDark = theme === "dark";
+  const isDark = theme === 'dark';
   // 创建一个新的对象，包含所有的 props 及其默认值
   const mergedProps = {
     theme,
@@ -46,78 +46,13 @@ const Report: FC<ReportProps> = (props) => {
     name,
   };
 
-  // 定义 CSS 字符串
-  const css = `
-
-.YourHighlightClass {
-  padding: 0 !important;
-  color: white !important;
-  background: rgb(244, 176, 27) !important;
-}
-
-/*新版canyon-report*/
-
-.line-number-wrapper {
-  display: flex;
-  /*从右边开始*/
-  justify-content: flex-end;
-  /*text-align: right;*/
-}
-
-.line-number-wrapper .line-number {
-  /*width: 60px;*/
-  padding-right: 5px;
-}
-
-.line-number-wrapper .line-change {
-  width: 4px;
-  /*display: block;*/
-}
-
-.line-number-wrapper .line-coverage {
-  width: 60px;
-  padding-right: 5px;
-  color: rgba(0, 0, 0, 0.5);
-}
-
-.dark .line-number-wrapper .line-coverage {
-  width: 60px;
-  padding-right: 5px;
-  color: #eaeaea;
-}
-
-/*额外的*/
-.content-class-no-found {
-  background: #f6c6ce;
-}
-
-.dark .content-class-no-found {
-  background: #7A5474;
-}
-
-    `;
-
-  useEffect(() => {
-    // 创建一个 <style> 元素
-    const styleElement = document.createElement("style");
-    // 将 CSS 字符串赋值给 <style> 元素的 innerHTML
-    styleElement.innerHTML = css;
-    // 将 <style> 元素插入到 <head> 标签中
-    document.head.appendChild(styleElement);
-
-    // 组件卸载时的清理操作
-    return () => {
-      document.head.removeChild(styleElement);
-    };
-  }, []);
-
   return (
     <LanguageProvider language={language}>
       <ConfigProvider
         locale={languages[language]}
         theme={{
           token: {
-            colorPrimary: "#0071c2",
+            colorPrimary: '#0071c2',
             borderRadius: 2,
           },
           algorithm: isDark ? [darkAlgorithm] : [],
