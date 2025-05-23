15a3dc412cfb6ea17e5ea5f296e19b83137a19e3
zhangtao25 <wr.zhang25@gmail.com>
2025-05-23 22:31:14 +0800

    chore: daily development

--- packages/report/src/components/widgets/CoverageDetail.tsx
+++ packages/report/src/components/widgets/CoverageDetail.tsx
@@ -1,5 +1,5 @@
 import { FC, useEffect, useMemo, useRef, useState } from 'react';
-import { initCanyonSpa } from 'canyon-report-spa';
+// import { initCanyonSpa } from 'canyon-report-spa';
 
 import { theme } from 'antd';
 const { useToken } = theme;
@@ -9,16 +9,45 @@ const FileCoverageDetail: FC<{
   fileCoverage: any;
 }> = ({ fileContent, fileCoverage, fileCodeChange }) => {
   useEffect(() => {
-    initCanyonSpa(document.getElementById('box'), {
-      coverage: fileCoverage,
-      content: fileContent,
-      diff:fileCodeChange
-    });
+    // initCanyonSpa(document.getElementById('box'), {
+    //   coverage: fileCoverage,
+    //   content: fileContent,
+    //   diff:fileCodeChange
+    // });
+
+    // 创建 script 元素
+    const script = document.createElement('script');
+    // @ts-ignore
+    script.src = (window.UNPKG_URL || 'https://unpkg.com') +
+      '/canyon-report-spa/dist/index.umd.cjs'; // 替换为实际 URL
+    // script.async = true; // 异步加载（默认）
+
+    // 设置加载成功和失败的回调
+    script.onload = () => {
+      console.log('脚本加载成功');
+      // 这里可以执行依赖该脚本的代码
+      // @ts-ignore
+      CanyonReportSpa.initCanyonSpa(
+        document.getElementById('canyon-report-box'),
+        {
+          coverage: fileCoverage,
+          content: fileContent,
+          diff: fileCodeChange,
+        },
+      );
+    };
+
+    script.onerror = () => {
+      console.error('脚本加载失败');
+    };
+
+    // 将脚本添加到页面
+    document.head.appendChild(script);
   }, []);
 
   return (
     <div>
-      <div id={'box'} style={{width:'100%'}}></div>
+      <div id={'canyon-report-box'} style={{ width: '100%' }}></div>
     </div>
   );
 };
