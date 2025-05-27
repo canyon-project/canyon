feb820331d065b72e8f285cfbf5df324f1bfeb65
zhangtao25 <wr.zhang25@gmail.com>
2025-05-27 20:15:50 +0800

    chore: daily development

--- packages/simple/src/app.module.ts
+++ packages/simple/src/app.module.ts
@@ -12,10 +12,25 @@ import { CoverageModule } from './apps/coverage/coverage.module';
 // import { ProjectModule } from './apps/project/project.module';
 // import { SourcecodeModule } from './apps/sourcecode/sourcecode.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import {SimpleCoverage} from "./apps/coverage/entities/simple-coverage.entity";
+import { SimpleCoverage } from './apps/coverage/entities/simple-coverage.entity';
+
+const DATABASE_URL = process.env.DATABASE_URL;
+// @ts-ignore
+const [, username, password, host, port, database] = DATABASE_URL.match(
+  /postgres:\/\/([^:]+):([^@]+)@([^:]+):(\d+)\/([^?]+)/,
+);
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: host,
+      port: port,
+      username: username,
+      password: password,
+      database: database,
+      entities: [SimpleCoverage],
+    }),
     // ClickHouseModule,
     // PrismaModule,
     // apps
