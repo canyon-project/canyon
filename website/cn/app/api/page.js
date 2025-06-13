42e3a1fc9ae318f6990e4400af8081d99cc98e5a
zhangtao25 <wr.zhang25@gmail.com>
2025-06-13 18:10:35 +0800

    chore: daily development

--- website/cn/app/api/page.js
+++ website/cn/app/api/page.js
@@ -16,7 +16,7 @@ export default function Page() {
   <title>Document</title>
 </head>
 <body>
-<redoc spec-url="http://petstore.swagger.io/v2/swagger.json"></redoc>
+<redoc spec-url="https://app.canyonjs.org/swagger/json"></redoc>
 <script src="https://cdn.redoc.ly/redoc/latest/bundles/redoc.standalone.js"> </script>
 </body>
 </html>
