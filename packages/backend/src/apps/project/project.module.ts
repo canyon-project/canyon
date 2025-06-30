29d2d594415183ac40103eeab0b750fff3f2f412
Travis Zhang <wr.zhang25@gmail.com>
2025-06-30 14:24:13 +0800

    chore: daily development

--- packages/backend/src/apps/project/project.module.ts
+++ packages/backend/src/apps/project/project.module.ts
@@ -4,7 +4,6 @@ import { ProjectController } from './project.controller';
 import { GetRepoCommitsByRepoIdServices } from './services/get-repo-commits-by-repo-id.services';
 import { GetRepoCommitByCommitShaServices } from './services/get-repo-commit-by-commit-sha.services';
 import { CoverageFinalService } from '../coverage/services/core/coverage-final.service';
-import { CoverageMapService } from '../coverage/services/core/coverage-map.service';
 import { RepoService } from './services/repo.service';
 @Module({
   imports: [PrismaModule],
@@ -13,7 +12,6 @@ import { RepoService } from './services/repo.service';
     GetRepoCommitsByRepoIdServices,
     GetRepoCommitByCommitShaServices,
     CoverageFinalService,
-    CoverageMapService,
     RepoService,
   ],
   exports: [],
