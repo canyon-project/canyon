ad3ce921937898cb6b687d731f36b5030b193d9e
Travis Zhang <wr.zhang25@gmail.com>
2025-08-02 00:36:50 +0800

    chore: update environment configuration and add utility scripts

--- packages/frontend/src/pages/home.tsx
+++ packages/frontend/src/pages/home.tsx
@@ -1,6 +1,13 @@
 import {Button} from "antd";
+import {useEffect} from "react";
+import axios from "axios";
 
 const Home = () => {
+  useEffect(() => {
+    axios.get(`/api/v1/coverage`).then(r=>{
+      console.log(r.data)
+    })
+  }, []);
   return <div>
       <Button type={'primary'}>primary</Button>
   </div>
