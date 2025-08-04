49cecd8e5dfde33f1c7c5a1c30528937d9498221
Travis Zhang <wr.zhang25@gmail.com>
2025-08-04 16:48:21 +0800

    fix: add comments for environment variable configuration in Babel

--- examples/webpack-babel-ts/babel.config.js
+++ examples/webpack-babel-ts/babel.config.js
@@ -3,6 +3,13 @@ module.exports = {
     'canyon',{
       keepMap: true,
       ci: true,
+      // 以下配置支持显式传入环境变量
+      // projectID: process.env.CI_PROJECT_ID,
+      // sha: process.env.CI_COMMIT_SHA,
+      // dsn: process.env.DSN,
+      // reporter: process.env.REPORTER,
+      // branch: process.env.CI_COMMIT_BRANCH,
+      // instrumentCwd: process.cwd(),
     }
   ]]
 };
