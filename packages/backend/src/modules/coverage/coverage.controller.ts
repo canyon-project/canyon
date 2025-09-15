3ad28895e6e29e6b69c79f8bbd7dbf68a58235c4
Travis Zhang <wr.zhang25@gmail.com>
2025-09-15 14:47:25 +0800

    feat: update getSummaryMap

--- packages/backend/src/modules/coverage/coverage.controller.ts
+++ packages/backend/src/modules/coverage/coverage.controller.ts
@@ -1,4 +1,6 @@
 import { BadRequestException, Controller, Get, Query } from '@nestjs/common';
+// @ts-expect-error canyon-data 可能缺少类型导出，但运行时可用
+import { genSummaryMapByCoverageMap, getSummaryByPath } from 'canyon-data';
 import {
   type CoverageFileMapEntry,
   summarizeCoverageFromMap,
@@ -6,7 +8,6 @@ import {
 import { MapQueryDto } from './dto/map.dto';
 import { SummaryMapQueryDto } from './dto/summary-map.dto';
 import { CoverageMapForCommitService } from './services/coverage-map-for-commit.service';
-// import { CoverageMapService } from './services/coverage.map.service';
 
 @Controller('api/coverage')
 export class CoverageController {
@@ -14,51 +15,51 @@ export class CoverageController {
     private readonly coverageMapForCommitService: CoverageMapForCommitService,
   ) {}
 
-  // @Get('summary/map')
-  // async getSummaryMap(@Query() q: SummaryMapQueryDto) {
-  //   const { subject } = q;
-  //   const percent = (covered: number, total: number) =>
-  //     total <= 0
-  //       ? 100.0
-  //       : Math.floor((1000 * 100 * covered) / total / 10) / 100;
-  //   switch (subject) {
-  //     case 'commit':
-  //     case 'commits': {
-  //       const map = await this.map.getMapForCommit({
-  //         provider: q.provider,
-  //         repoID: q.repoID,
-  //         sha: q.subjectID,
-  //         buildProvider: q.buildProvider,
-  //         buildID: q.buildID,
-  //         reportProvider: q.reportProvider,
-  //         reportID: q.reportID,
-  //         filePath: q.filePath,
-  //         compareTarget: q.compareTarget,
-  //         onlyChanged: String(q.onlyChanged || '').toLowerCase() === 'true',
-  //       });
-  //       return summarizeCoverageFromMap(
-  //         map as Record<string, CoverageFileMapEntry>,
-  //         percent,
-  //       );
-  //     }
-  //     // case 'pull':
-  //     // case 'pulls': {
-  //     //   const map = await this.map.getMapForPull({
-  //     //     provider: q.provider,
-  //     //     repoID: q.repoID,
-  //     //     pullNumber: q.subjectID,
-  //     //     filePath: q.filePath,
-  //     //     mode: q.mode,
-  //     //   });
-  //     //   return summarizeCoverageFromMap(
-  //     //     map as Record<string, CoverageFileMapEntry>,
-  //     //     percent,
-  //     //   );
-  //     // }
-  //     default:
-  //       throw new BadRequestException('invalid subject');
-  //   }
-  // }
+  @Get('summary/map')
+  async getSummaryMap(@Query() q: SummaryMapQueryDto) {
+    const { subject } = q;
+    const percent = (covered: number, total: number) =>
+      total <= 0
+        ? 100.0
+        : Math.floor((1000 * 100 * covered) / total / 10) / 100;
+    switch (subject) {
+      case 'commit':
+      case 'commits': {
+        const map = await this.coverageMapForCommitService.invoke({
+          provider: q.provider,
+          repoID: q.repoID,
+          sha: q.subjectID,
+          buildProvider: q.buildProvider,
+          buildID: q.buildID,
+          reportProvider: q.reportProvider,
+          reportID: q.reportID,
+          filePath: q.filePath,
+          compareTarget: q.compareTarget,
+          onlyChanged: String(q.onlyChanged || '').toLowerCase() === 'true',
+        });
+
+        const summary = genSummaryMapByCoverageMap(map, []);
+
+        return summary;
+      }
+      // case 'pull':
+      // case 'pulls': {
+      //   const map = await this.map.getMapForPull({
+      //     provider: q.provider,
+      //     repoID: q.repoID,
+      //     pullNumber: q.subjectID,
+      //     filePath: q.filePath,
+      //     mode: q.mode,
+      //   });
+      //   return summarizeCoverageFromMap(
+      //     map as Record<string, CoverageFileMapEntry>,
+      //     percent,
+      //   );
+      // }
+      default:
+        throw new BadRequestException('invalid subject');
+    }
+  }
 
   @Get('map')
   async getMap(@Query() q: MapQueryDto) {
