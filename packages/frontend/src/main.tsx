323ec8e6c8e42431f696256826a5f5f797efc460
Travis Zhang <wr.zhang25@gmail.com>
2025-10-29 14:21:54 +0800

    chore: daily development

--- packages/frontend/src/main.tsx
+++ packages/frontend/src/main.tsx
@@ -1,10 +1,28 @@
-import { StrictMode } from 'react';
+import '@ant-design/v5-patch-for-react-19';
+// import './i18n.ts';
+// import './useWorker.ts';
+// import { createRoot } from 'react-dom/client';
+// import './index.css';
+import {
+  ApolloClient,
+  // ApolloProvider,
+  HttpLink,
+  InMemoryCache,
+} from '@apollo/client';
+import { ApolloProvider } from '@apollo/client/react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App.tsx';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const client = new ApolloClient({
+  link: new HttpLink({ uri: '/graphql' }),
+  cache: new InMemoryCache(),
+});
+
+createRoot(document.getElementById('root') as HTMLElement).render(
+  <BrowserRouter>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </BrowserRouter>,
 );
