9e1776097b4aa8358cb34a10a44142c12d648044
Travis Zhang <wr.zhang25@gmail.com>
2025-10-29 14:32:54 +0800

    chore: daily development

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -1,15 +1,20 @@
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { RepoModule } from './repo/repo.module';
-
 @Module({
   imports: [
     PrismaModule,
     RepoModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'client'),
+      exclude: ['/graphql'], // 这样就不会触发 path-to-regexp 解析错误
+    }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       autoSchemaFile: 'schema.gql',
       driver: ApolloDriver,
