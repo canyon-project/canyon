f3810762949514970b986450a6e0b9f6a5451ec9
Travis Zhang <wr.zhang25@gmail.com>
2025-10-29 14:29:14 +0800

    chore: daily development

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,14 +1,21 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-import './App.css';
+// import './App.css';
+
+import { useQuery } from '@apollo/client/react';
+import { RepoDocument } from '@/helpers/backend/gen/graphql.ts';
 
 function App() {
   // const [count, setCount] = useState(0)
-
+  const { data } = useQuery(RepoDocument, {
+    variables: {
+      collectionID: 's',
+    },
+  });
   return (
     <>
-      <div>乌鸦坐飞机</div>
+      <div>{JSON.stringify(data || {})}</div>
     </>
   );
 }
