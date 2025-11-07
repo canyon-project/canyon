c636f794aee3c760ce08e028f4994ef0d266f189
Travis Zhang <wr.zhang25@gmail.com>
2025-11-07 14:21:42 +0800

    chore: daily development

--- packages/frontend/src/main.tsx
+++ packages/frontend/src/main.tsx
@@ -5,17 +5,35 @@ import './i18n.ts';
 import './index.css';
 import {
   ApolloClient,
+  CombinedGraphQLErrors,
+  from,
   // ApolloProvider,
   HttpLink,
   InMemoryCache,
 } from '@apollo/client';
+import { ErrorLink } from '@apollo/client/link/error';
 import { ApolloProvider } from '@apollo/client/react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.tsx';
 
+// const httpLink = ``
+
+const errorLink = new ErrorLink(({ error }) => {
+  if (CombinedGraphQLErrors.is(error)) {
+    error.errors.forEach(({ message, locations, path }) => {
+      console.log(
+        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+      );
+    });
+  } else {
+    console.error('[Network error]:', error);
+  }
+});
+const httpLink = new HttpLink({ uri: '/graphql' });
+
 const client = new ApolloClient({
-  link: new HttpLink({ uri: '/graphql' }),
+  link: from([errorLink, httpLink]),
   cache: new InMemoryCache(),
 });
 
