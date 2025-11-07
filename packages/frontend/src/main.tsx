5f8c759a21a8a683c3eea8aa05fac9b032ea7bf7
Travis Zhang <wr.zhang25@gmail.com>
2025-11-07 14:29:12 +0800

    chore: daily development

--- packages/frontend/src/main.tsx
+++ packages/frontend/src/main.tsx
@@ -1,39 +1,37 @@
 import '@ant-design/v5-patch-for-react-19';
 import './i18n.ts';
-// import './useWorker.ts';
-// import { createRoot } from 'react-dom/client';
+import './useWorker.ts';
 import './index.css';
 import {
   ApolloClient,
+  ApolloLink,
   CombinedGraphQLErrors,
-  from,
-  // ApolloProvider,
   HttpLink,
   InMemoryCache,
 } from '@apollo/client';
 import { ErrorLink } from '@apollo/client/link/error';
 import { ApolloProvider } from '@apollo/client/react';
+import { message } from 'antd';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.tsx';
 
-// const httpLink = ``
-
 const errorLink = new ErrorLink(({ error }) => {
   if (CombinedGraphQLErrors.is(error)) {
-    error.errors.forEach(({ message, locations, path }) => {
-      console.log(
-        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+    error.errors.forEach(({ message: msg, locations, path }) => {
+      console.error(
+        `[GraphQL error]: msg: ${msg}, Location: ${locations}, Path: ${path}`,
       );
+      message.error(`[GraphQL error]: msg: ${msg}, Path: ${path}`);
     });
   } else {
-    console.error('[Network error]:', error);
+    message.error(`[Network error]:${error}`);
   }
 });
 const httpLink = new HttpLink({ uri: '/graphql' });
 
 const client = new ApolloClient({
-  link: from([errorLink, httpLink]),
+  link: ApolloLink.from([errorLink, httpLink]),
   cache: new InMemoryCache(),
 });
 
