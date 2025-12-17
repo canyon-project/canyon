77eb3ce2387f80cc95b31e3f27059bc986415e6d
wr.zhang25 <wr.zhang25@gmail.com>
2025-12-17 22:22:16 +0800

    chore: daily development

--- packages/backend/src/app.controller.ts
+++ packages/backend/src/app.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
-import type { AppService } from './app.service';
+import { AppService } from './app.service';
 
 @ApiTags('app')
 @Controller()
