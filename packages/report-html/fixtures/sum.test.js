7f4c5da829bd0a31cc26ec9a97dbcb637055b15e
Travis Zhang <wr.zhang25@gmail.com>
2025-12-18 15:17:51 +0800

    chore: daily development

--- packages/report-html/fixtures/sum.test.js
+++ packages/report-html/fixtures/sum.test.js
@@ -1,4 +1,4 @@
-import { expect, test } from 'vitest';
+import { expect, test } from '@rstest/core';
 import { sum } from './sum.js';
 
 test('adds 1 + 2 to equal 3', () => {
--- packages/report-html/rstest.config.ts
+++ packages/report-html/rstest.config.ts
@@ -3,6 +3,14 @@ import { defineConfig } from '@rstest/core';
 export default defineConfig({
   coverage: {
     provider: 'istanbul',
-    reporters: ['json', '@canyonjs/report-html'], // 直接调试的是当前的index.js入口
+    reporters: [
+      'json',
+      [
+        '@canyonjs/report-html',
+        {
+          diff: [],
+        },
+      ],
+    ], // 直接调试的是当前的index.js入口
   },
 });
