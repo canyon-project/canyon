05cdd68439ee3350605fdfeff16bcd979f196f27
Travis Zhang <wr.zhang25@gmail.com>
2025-12-18 15:15:34 +0800

    chore: daily development

---
+++ packages/report-html/rstest.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from '@rstest/core';
+
+export default defineConfig({
+  coverage: {
+    provider: 'istanbul',
+    reporters: ['json', '@canyonjs/report-html'], // 直接调试的是当前的index.js入口
+  },
+});
