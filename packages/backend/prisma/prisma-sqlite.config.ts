31664a0b4ffe5b70226f793f189cce464dbc3bfd
Travis Zhang <wr.zhang25@gmail.com>
2025-12-22 19:24:20 +0800

    chore: daily development

--- packages/backend/prisma/prisma-sqlite.config.ts
+++ packages/backend/prisma/prisma-sqlite.config.ts
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import { defineConfig } from 'prisma/config';
 
 export default defineConfig({
-  schema: 'prisma/schema-sqlite.prisma',
+  schema: 'schema-sqlite.prisma',
   datasource: {
     url: 'file:local.db',
   },
