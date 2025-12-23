fd61dffcd3207d14600f7bbba06b345277b66a85
Travis Zhang <wr.zhang25@gmail.com>
2025-12-23 11:21:47 +0800

    feat: define LogMessage

--- packages/backend/src/logger/index.ts
+++ packages/backend/src/logger/index.ts
@@ -1,3 +1,9 @@
-export function logger(data) {
+export interface LogMessage {
+  type: string;
+  title: string;
+  message: string;
+  addInfo?: object;
+}
+export function logger(data: LogMessage) {
   console.log(data);
 }
