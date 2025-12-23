49bdfc38e5eeccfce9cd81d1b4b57d0d8cec0943
Travis Zhang <wr.zhang25@gmail.com>
2025-12-23 14:37:12 +0800

    feat: add ui

--- packages/backend/src/health/health.module.ts
+++ packages/backend/src/health/health.module.ts
@@ -3,7 +3,6 @@ import { TerminusModule } from '@nestjs/terminus';
 import { HealthController } from './health.controller';
 import { RuntimeController } from './runtime.controller';
 
-console.log('1');
 @Module({
   imports: [TerminusModule],
   controllers: [HealthController, RuntimeController],
