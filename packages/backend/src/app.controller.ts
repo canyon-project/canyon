4a3139ae77aa8fcb7a48a0c147ca566153fe8d01
Travis Zhang <wr.zhang25@gmail.com>
2025-12-23 11:04:20 +0800

    chore: daily development

--- packages/backend/src/app.controller.ts
+++ packages/backend/src/app.controller.ts
@@ -8,7 +8,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get('vi/health')
-  @ApiOperation({ summary: '获取欢迎消息' })
+  @ApiOperation({ summary: '获取欢迎消' })
   @ApiResponse({ status: 200, description: '返回欢迎消息', type: String })
   getHello(): string {
     return this.appService.getHello();
