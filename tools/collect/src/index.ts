0d090b62dc45add3c40a9066e9dcc263a0849a75
Travis Zhang <wr.zhang25@gmail.com>
2025-12-24 19:01:12 +0800

    chore: daily development

--- tools/collect/src/index.ts
+++ tools/collect/src/index.ts
@@ -1,1 +1,23 @@
-console.log('hi');
+(() => {
+  const sendCoverage = () => {
+    const coverageFirstValue = Object.values(window.__coverage__ || {})[0];
+    if (coverageFirstValue) {
+      fetch(coverageFirstValue.dsn, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          coverage: window.__coverage__,
+          ...coverageFirstValue,
+        }),
+      });
+    }
+  };
+
+  document.addEventListener('visibilitychange', () => {
+    if (document.visibilityState === 'hidden') {
+      sendCoverage();
+    }
+  });
+})();
