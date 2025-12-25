30bcbbbd5162c91f6db0d2956880a6079dd673d4
Travis Zhang <wr.zhang25@gmail.com>
2025-12-25 14:12:59 +0800

    feat: update

--- tools/extension/vite.config.ts
+++ tools/extension/vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,4 +10,4 @@ export default defineConfig({
       },
     }),
   ],
-})
+});
