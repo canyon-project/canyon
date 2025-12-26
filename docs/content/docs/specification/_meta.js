49bc1ec3e8da8ce7f3de5f4e1e7b94113ab4402a
Travis Zhang <wr.zhang25@gmail.com>
2025-12-26 17:36:34 +0800

    feat: update

---
+++ docs/content/docs/specification/_meta.js
@@ -0,0 +1,7 @@
+export default {
+  'data-model': 'Coverage Data Model',
+  'identity-model': 'Identity & Hash Model',
+  'merge-semantics': 'Merge Semantics',
+  protocol: 'Map & Hit Protocol',
+  'fail-open': 'Fail-Open Semantics',
+};
