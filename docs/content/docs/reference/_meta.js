7c7c40a9580c4d4d9eb905cfe7011ae37f8addb1
Travis Zhang <wr.zhang25@gmail.com>
2025-12-29 11:11:10 +0800

    chore: daily development

--- docs/content/docs/reference/_meta.js
+++ docs/content/docs/reference/_meta.js
@@ -1,4 +1,5 @@
 export default {
+  compatibility: 'Toolchain Compatibility',
   provider: 'Supported CI Providers',
   'istanbul-data-structure': 'istanbuljs Data Structure Dictionary',
   'custom-reporting': 'Custom Data Reporting',
