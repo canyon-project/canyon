d257a54bf9c21b4e44c5f82450e06d80ce1b96c0
Travis Zhang <wr.zhang25@gmail.com>
2026-01-28 14:51:23 +0800

    docs: add @canyonjs/report documentation and update git-diff usage examples

--- docs/content/docs/ecosystem/_meta.js
+++ docs/content/docs/ecosystem/_meta.js
@@ -2,6 +2,7 @@ export default {
   'babel-plugin': '@canyonjs/babel-plugin',
   cli: '@canyonjs/cli',
   'report-html': '@canyonjs/report-html',
+  'report': '@canyonjs/report',
   'git-diff': '@canyonjs/git-diff',
   'playwright': '@canyonjs/playwright',
 };
