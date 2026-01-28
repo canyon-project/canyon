883bb0d84fab8ac9077f9d6c38f9cc218858b545
Travis Zhang <wr.zhang25@gmail.com>
2026-01-28 14:10:41 +0800

    chore: update rootPath in ServeStaticModule configuration to point to the new client directory

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -1,4 +1,3 @@
-import { existsSync } from 'node:fs';
 import { join } from 'node:path';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
@@ -24,9 +23,7 @@ import { RepoModule } from './repo/repo.module';
     RepoModule,
     CodeModule,
     ServeStaticModule.forRoot({
-      rootPath: existsSync(join(__dirname, '../../frontend', 'dist'))
-        ? join(__dirname, '../../frontend', 'dist')
-        : join(__dirname, '..', 'public'),
+      rootPath: join(__dirname, '..', 'client'),
     }),
   ],
   controllers: [],
