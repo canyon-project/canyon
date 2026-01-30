cf281094b1e00d7ed30f5500d54df94c05838dc6
Travis Zhang <wr.zhang25@gmail.com>
2026-01-30 11:45:55 +0800

    chore: remove API documentation and related metadata files

--- docs/content/docs/api/_meta.js
+++
@@ -1,4 +0,0 @@
-export default {
-  overview: 'Overview',
-  coverage: 'Coverage',
-};
--- docs/content/docs/reference/_meta.js
+++
@@ -1,5 +0,0 @@
-export default {
-  'istanbul-data-structure': 'istanbuljs 数据结构字典',
-  'custom-reporting': '自定义数据报告',
-  'report-period-component': '报告期组件说明',
-};
