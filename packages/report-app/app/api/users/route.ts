d382ff6320fe030d2fbc3cd5ee7fb8f7e0612662
Travis Zhang <wr.zhang25@gmail.com>
2026-01-30 17:52:06 +0800

    feat: configure dotenv for environment variables and rename dev script in package.json

---
+++ packages/report-app/app/api/users/route.ts
@@ -0,0 +1,16 @@
+import { NextResponse } from 'next/server';
+import { prisma } from '../../../lib/prisma';
+
+export async function GET() {
+  const repoList = await prisma.repo.findMany({
+    where:{}
+  })
+  // 如果表不存在，返回空数组（表可能还未创建）
+  return NextResponse.json({
+    success: true,
+    data: repoList,
+    total: 0,
+    message: 'User table does not exist',
+  });
+
+}
--- packages/report-app/next.config.ts
+++ packages/report-app/next.config.ts
@@ -1,5 +1,10 @@
 import type { NextConfig } from "next";
+import dotenv from 'dotenv';
+import path from "node:path";
 
+dotenv.config({
+  path: [path.resolve(__dirname, '../../.env')],
+});
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
