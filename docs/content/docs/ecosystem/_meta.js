fec479e50292f054f1edfb663f057656f8d48210
Travis Zhang <wr.zhang25@gmail.com>
2026-02-09 17:22:16 +0800

    chore: remove unused report-html dependency from meta configuration

--- docs/content/docs/ecosystem/_meta.js
+++ docs/content/docs/ecosystem/_meta.js
@@ -1,7 +1,6 @@
 export default {
   'babel-plugin': '@canyonjs/babel-plugin',
   cli: '@canyonjs/cli',
-  'report-html': '@canyonjs/report-html',
   report: '@canyonjs/report',
   'git-diff': '@canyonjs/git-diff',
   playwright: '@canyonjs/playwright',
