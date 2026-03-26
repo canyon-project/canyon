38d2180f13cbadac8e722635be6abb81623f1e64
Travis Zhang <wr.zhang25@gmail.com>
2026-03-26 19:49:02 +0800

    feat: update babel-plugin version to 1.0.6 and add empty string metadata keys to visitor program exit

--- plugins/babel-plugin/src/visitor-program-exit.ts
+++ plugins/babel-plugin/src/visitor-program-exit.ts
@@ -182,6 +182,23 @@ export function visitorProgramExit(
               types.stringLiteral(buildHash),
             );
             objectProperties.push(buildHashProperty);
+
+            const emptyStringMetadataKeys = [
+              "sha",
+              "instrumentCwd",
+              "dsn",
+              "reporter",
+              "branch",
+              "compareTarget",
+              "provider",
+              "repoID",
+            ] as const;
+
+            emptyStringMetadataKeys.forEach((key) => {
+              objectProperties.push(
+                types.objectProperty(types.identifier(key), types.stringLiteral("")),
+              );
+            });
           }
         }
       },
