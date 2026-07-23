030f7583664365c768f02fe753a62fb2b6e2149e
Travis Zhang <wr.zhang25@gmail.com>
2026-07-23 15:28:45 +0800

    feat: update report-component to version 1.0.29 and refactor App component to use CanyonReportApp

--- packages/report-component/src/index.ts
+++ packages/report-component/src/index.ts
@@ -1,3 +1,10 @@
 export { CanyonReport, type CanyonReportProps, default } from "./CanyonReport";
+export { CanyonReportApp } from "./CanyonReportApp";
+export type {
+  CanyonReportAppProps,
+  CanyonReportAppFile,
+  FileDataResponse,
+  DataSourceItem,
+} from "./types";
 // src/index.tsx
 import "./index.css"; // 导入CSS
